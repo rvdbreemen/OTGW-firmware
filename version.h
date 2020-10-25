863499831501cbbb306d2b2ad4a3ac1dd3c1aae6
Robert van den Breemen <robert@vandenbreemen.net>
2020-10-25 23:56:07 +0100

    before update 0.1.0

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 //The version number conforms to semver.org format
 #define _VERSION_MAJOR 0
-#define _VERSION_MINOR 0  
-#define _VERSION_PATCH 1
+#define _VERSION_MINOR 1  
+#define _VERSION_PATCH 0
 #define _VERSION_BUILD 24
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "25-10-2020"
