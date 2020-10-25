e3ff6688de530c3bec07585735b784317c4d483c
Robert van den Breemen <robert@vandenbreemen.net>
2020-10-25 20:48:57 +0100

    before update 0.0.1

---
+++ version.h
@@ -0,0 +1,14 @@
+//The version number conforms to semver.org format
+#define _VERSION_MAJOR 0
+#define _VERSION_MINOR 0  
+#define _VERSION_PATCH 1
+#define _VERSION_BUILD 2
+//#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
+#define _VERSION_DATE "25-10-2020"
+#define _VERSION_TIME "20:47:06"
+#define _SEMVER_CORE "0.0.1"
+#define _SEMVER_BUILD "0.0.1+2"
+#define _SEMVER_FULL "0.0.1+2"
+#define _SEMVER_NOBUILD "0.0.1 (25-10-2020)"
+#define _VERSION "0.0.1+2 (25-10-2020)"
+//The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
