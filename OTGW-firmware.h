32c313e9b0a5b1338e93f427566152892327c851
Robert van den Breemen <robert@vandenbreemen.net>
2020-11-20 00:44:15 +0100

    update version to 0.3.0

--- OTGW-firmware.h
+++ OTGW-firmware.h
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program  : OTGW-firmware.h
-**  Version  : v0.2.0
+**  Version  : v0.3.0
 **
 **  Copyright (c) 2020 Robert van den Breemen
 **  Borrowed from OpenTherm library from: 
