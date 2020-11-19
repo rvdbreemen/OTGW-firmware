d97ed68f3e6382c57c784ec314eaa8c8e4a4150a
Robert van den Breemen <robert@vandenbreemen.net>
2020-11-20 00:48:31 +0100

    Pushing 0.3.0
    OTmon interface implemented on port 1023

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 3  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 179
+#define _VERSION_BUILD 180
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "20-11-2020"
-#define _VERSION_TIME "00:43:02"
+#define _VERSION_TIME "00:47:54"
 #define _SEMVER_CORE "0.3.0"
-#define _SEMVER_BUILD "0.3.0+179"
-#define _SEMVER_FULL "0.3.0+179"
+#define _SEMVER_BUILD "0.3.0+180"
+#define _SEMVER_FULL "0.3.0+180"
 #define _SEMVER_NOBUILD "0.3.0 (20-11-2020)"
-#define _VERSION "0.3.0+179 (20-11-2020)"
+#define _VERSION "0.3.0+180 (20-11-2020)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
