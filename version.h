5a88d54d7efc30be90b18fec5462f994daa52917
Robert van den Breemen <robert@vandenbreemen.net>
2020-12-10 09:49:15 +0100

    Fixed the wifimanager code

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 3  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 186
+#define _VERSION_BUILD 189
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "10-12-2020"
-#define _VERSION_TIME "00:24:02"
+#define _VERSION_TIME "09:45:56"
 #define _SEMVER_CORE "0.3.0"
-#define _SEMVER_BUILD "0.3.0+186"
-#define _SEMVER_FULL "0.3.0+186"
+#define _SEMVER_BUILD "0.3.0+189"
+#define _SEMVER_FULL "0.3.0+189"
 #define _SEMVER_NOBUILD "0.3.0 (10-12-2020)"
-#define _VERSION "0.3.0+186 (10-12-2020)"
+#define _VERSION "0.3.0+189 (10-12-2020)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
