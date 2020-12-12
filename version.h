557ebb739138c6168a267d2e4e50f95ec8141069
Robert van den Breemen <robert@vandenbreemen.net>
2020-12-12 01:44:05 +0100

    v0.4.0 - working rest api - fixed serial output (1023)

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 4  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 253
+#define _VERSION_BUILD 254
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "12-12-2020"
-#define _VERSION_TIME "01:33:39"
-#define _SEMVER_CORE "0.3.1"
-#define _SEMVER_BUILD "0.3.1+253"
-#define _SEMVER_FULL "0.3.1+253"
-#define _SEMVER_NOBUILD "0.3.1 (12-12-2020)"
-#define _VERSION "0.3.1+253 (12-12-2020)"
+#define _VERSION_TIME "01:38:16"
+#define _SEMVER_CORE "0.4.0"
+#define _SEMVER_BUILD "0.4.0+254"
+#define _SEMVER_FULL "0.4.0+254"
+#define _SEMVER_NOBUILD "0.4.0 (12-12-2020)"
+#define _VERSION "0.4.0+254 (12-12-2020)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
