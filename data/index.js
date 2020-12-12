a3d0661f7a23396a935a1d9e3b3c0d2b689f0ee7
Robert van den Breemen <robert@vandenbreemen.net>
2020-12-12 01:35:38 +0100

    update version to 0.3.1

--- data/index.js
+++ data/index.js
@@ -1,7 +1,7 @@
 /*
 ***************************************************************************  
 **  Program  : index.js, part of ESP_ticker
-**  Version  : v0.3.0
+**  Version  : v0.3.1
 **
 **  Copyright (c) 2020 Willem Aandewiel
 **
