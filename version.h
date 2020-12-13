bfadaa96fb03c78b1a4c58b7a2699d74c245aa87
Robert van den Breemen <robert@vandenbreemen.net>
2020-12-13 23:15:46 +0100

    one more thing on the REST API

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 4  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 315
+#define _VERSION_BUILD 318
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "13-12-2020"
-#define _VERSION_TIME "22:58:33"
+#define _VERSION_TIME "23:15:12"
 #define _SEMVER_CORE "0.4.2"
-#define _SEMVER_BUILD "0.4.2+315"
-#define _SEMVER_FULL "0.4.2+315"
+#define _SEMVER_BUILD "0.4.2+318"
+#define _SEMVER_FULL "0.4.2+318"
 #define _SEMVER_NOBUILD "0.4.2 (13-12-2020)"
-#define _VERSION "0.4.2+315 (13-12-2020)"
+#define _VERSION "0.4.2+318 (13-12-2020)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
