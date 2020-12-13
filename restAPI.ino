82a75c982b1411babee747d5c7a66aabc6408e60
Robert van den Breemen <robert@vandenbreemen.net>
2020-12-13 11:12:05 +0100

    update version to 0.4.1

--- restAPI.ino
+++ restAPI.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program  : restAPI
-**  Version  : v0.4.0
+**  Version  : v0.4.1
 **
 **  Copyright (c) 2020 Robert van den Breemen
 **     based on Framework ESP8266 from Willem Aandewiel
