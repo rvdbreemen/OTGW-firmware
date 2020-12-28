b89ddf7432b641a25ebda6765d1e57cc49a7d70b
Robert van den Breemen <robert@vandenbreemen.net>
2020-12-29 00:52:58 +0100

    Small stuff, documentation, and more.

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 4  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 319
+#define _VERSION_BUILD 322
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "28-12-2020"
-#define _VERSION_TIME "21:02:05"
+#define _VERSION_DATE "29-12-2020"
+#define _VERSION_TIME "00:50:46"
 #define _SEMVER_CORE "0.4.2"
-#define _SEMVER_BUILD "0.4.2+319"
-#define _SEMVER_FULL "0.4.2+319"
-#define _SEMVER_NOBUILD "0.4.2 (28-12-2020)"
-#define _VERSION "0.4.2+319 (28-12-2020)"
+#define _SEMVER_BUILD "0.4.2+322"
+#define _SEMVER_FULL "0.4.2+322"
+#define _SEMVER_NOBUILD "0.4.2 (29-12-2020)"
+#define _VERSION "0.4.2+322 (29-12-2020)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
