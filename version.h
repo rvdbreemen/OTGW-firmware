8af4c0addd3215b4e7f17207bcfd7884fc1dd1cf
Robert van den Breemen <robert@vandenbreemen.net>
2021-01-20 12:46:21 +0100

    msgid 131 fixed

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 7  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 418
+#define _VERSION_BUILD 419
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "20-01-2021"
-#define _VERSION_TIME "12:40:22"
+#define _VERSION_TIME "12:45:28"
 #define _SEMVER_CORE "0.7.0"
-#define _SEMVER_BUILD "0.7.0+418"
-#define _SEMVER_FULL "0.7.0+418"
+#define _SEMVER_BUILD "0.7.0+419"
+#define _SEMVER_FULL "0.7.0+419"
 #define _SEMVER_NOBUILD "0.7.0 (20-01-2021)"
-#define _VERSION "0.7.0+418 (20-01-2021)"
+#define _VERSION "0.7.0+419 (20-01-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
