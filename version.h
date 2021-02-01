8650b0acc779ae2efd4b3ae377b87e8a918c5a08
Robert van den Breemen <robert@vandenbreemen.net>
2021-02-02 00:56:38 +0100

    seriously hunting bugs with Tim

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 7  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 496
+#define _VERSION_BUILD 497
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "02-02-2021"
-#define _VERSION_TIME "00:36:20"
+#define _VERSION_TIME "00:56:22"
 #define _SEMVER_CORE "0.7.3"
-#define _SEMVER_BUILD "0.7.3+496"
-#define _SEMVER_FULL "0.7.3+496"
+#define _SEMVER_BUILD "0.7.3+497"
+#define _SEMVER_FULL "0.7.3+497"
 #define _SEMVER_NOBUILD "0.7.3 (02-02-2021)"
-#define _VERSION "0.7.3+496 (02-02-2021)"
+#define _VERSION "0.7.3+497 (02-02-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
