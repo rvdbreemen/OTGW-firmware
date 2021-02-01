4605fbfa3ce0cb5723fdf2f1eea681053f8078a0
Robert van den Breemen <robert@vandenbreemen.net>
2021-02-02 00:22:46 +0100

    change back some stuff in define land

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 7  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 490
+#define _VERSION_BUILD 493
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "01-02-2021"
-#define _VERSION_TIME "23:02:06"
+#define _VERSION_DATE "02-02-2021"
+#define _VERSION_TIME "00:20:44"
 #define _SEMVER_CORE "0.7.3"
-#define _SEMVER_BUILD "0.7.3+490"
-#define _SEMVER_FULL "0.7.3+490"
-#define _SEMVER_NOBUILD "0.7.3 (01-02-2021)"
-#define _VERSION "0.7.3+490 (01-02-2021)"
+#define _SEMVER_BUILD "0.7.3+493"
+#define _SEMVER_FULL "0.7.3+493"
+#define _SEMVER_NOBUILD "0.7.3 (02-02-2021)"
+#define _VERSION "0.7.3+493 (02-02-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
