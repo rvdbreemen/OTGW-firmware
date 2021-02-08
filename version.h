39a5f86ac76ed02a7893891544e1151d5ccc6a03
Robert van den Breemen <robert@vandenbreemen.net>
2021-02-08 01:46:11 +0100

    fixed fetch version on reboot

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 7  
 #define _VERSION_PATCH 4
-#define _VERSION_BUILD 570
+#define _VERSION_BUILD 573
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "08-02-2021"
-#define _VERSION_TIME "01:34:48"
+#define _VERSION_TIME "01:44:04"
 #define _SEMVER_CORE "0.7.4"
-#define _SEMVER_BUILD "0.7.4+570"
-#define _SEMVER_FULL "0.7.4+570"
+#define _SEMVER_BUILD "0.7.4+573"
+#define _SEMVER_FULL "0.7.4+573"
 #define _SEMVER_NOBUILD "0.7.4 (08-02-2021)"
-#define _VERSION "0.7.4+570 (08-02-2021)"
+#define _VERSION "0.7.4+573 (08-02-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
