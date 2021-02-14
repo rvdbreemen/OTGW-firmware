b63ada699f3acd48c698727432360a0750d9a1c2
Robert van den Breemen <robert@vandenbreemen.net>
2021-02-14 14:31:21 +0100

    firmware list files api added

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 7  
 #define _VERSION_PATCH 5
-#define _VERSION_BUILD 586
+#define _VERSION_BUILD 608
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "10-02-2021"
-#define _VERSION_TIME "21:05:58"
+#define _VERSION_DATE "14-02-2021"
+#define _VERSION_TIME "14:27:40"
 #define _SEMVER_CORE "0.7.5"
-#define _SEMVER_BUILD "0.7.5+586"
-#define _SEMVER_FULL "0.7.5+586"
-#define _SEMVER_NOBUILD "0.7.5 (10-02-2021)"
-#define _VERSION "0.7.5+586 (10-02-2021)"
+#define _SEMVER_BUILD "0.7.5+608"
+#define _SEMVER_FULL "0.7.5+608"
+#define _SEMVER_NOBUILD "0.7.5 (14-02-2021)"
+#define _VERSION "0.7.5+608 (14-02-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
