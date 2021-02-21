3fff7b309a5196e06c96ee2a0f5fe6ced1aa9783
Robert van den Breemen <robert@vandenbreemen.net>
2021-02-21 14:12:51 +0100

    configurable heartbeat led

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 7  
 #define _VERSION_PATCH 6
-#define _VERSION_BUILD 680
+#define _VERSION_BUILD 686
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "21-02-2021"
-#define _VERSION_TIME "09:22:21"
+#define _VERSION_TIME "14:11:41"
 #define _SEMVER_CORE "0.7.6"
-#define _SEMVER_BUILD "0.7.6+680"
-#define _SEMVER_FULL "0.7.6+680"
+#define _SEMVER_BUILD "0.7.6+686"
+#define _SEMVER_FULL "0.7.6+686"
 #define _SEMVER_NOBUILD "0.7.6 (21-02-2021)"
-#define _VERSION "0.7.6+680 (21-02-2021)"
+#define _VERSION "0.7.6+686 (21-02-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
