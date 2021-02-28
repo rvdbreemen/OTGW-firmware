08f7955de4c7d670e571ec9812312b681c78ee1b
Robert van den Breemen <robert@vandenbreemen.net>
2021-02-28 21:18:20 +0100

    fixed ASF flags, it needed to be from the HB, not the LB
    The logic used for the ASF flags for the Web UI was wrong. It was taking the bit from the LB, not HB.

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 7  
 #define _VERSION_PATCH 8
-#define _VERSION_BUILD 718
+#define _VERSION_BUILD 719
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "27-02-2021"
-#define _VERSION_TIME "23:33:14"
+#define _VERSION_DATE "28-02-2021"
+#define _VERSION_TIME "21:17:23"
 #define _SEMVER_CORE "0.7.8"
-#define _SEMVER_BUILD "0.7.8+718"
-#define _SEMVER_FULL "0.7.8+718"
-#define _SEMVER_NOBUILD "0.7.8 (27-02-2021)"
-#define _VERSION "0.7.8+718 (27-02-2021)"
+#define _SEMVER_BUILD "0.7.8+719"
+#define _SEMVER_FULL "0.7.8+719"
+#define _SEMVER_NOBUILD "0.7.8 (28-02-2021)"
+#define _VERSION "0.7.8+719 (28-02-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
