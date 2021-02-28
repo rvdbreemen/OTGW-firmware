aefa7b8b7a2fc653431ec99414b88befa84ab18b
Robert van den Breemen <robert@vandenbreemen.net>
2021-02-28 22:24:50 +0100

    Revert "Revert "Revert "add HA device and thermostat entity through mqtt discovery."""
    This reverts commit 11db465c4bf15c340bd4c59350b13c0cd70789c6.

--- networkStuff.h
+++ networkStuff.h
@@ -196,8 +196,8 @@ String getMacAddress() {
   return String(baseMacChr);
 }
 
-String getUniqueId() {
-  String uniqueId = "otgw-"(String)getMacAddress();
+String getUniqueId(String name, String sufix) {
+  String uniqueId = (String)getMacAddress() + name + sufix;
   return String(uniqueId);
 }
 
