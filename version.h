7bf7637e5be93c23b71b8cac4352d92d104f78b6
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-04 23:27:47 +0100

    fixing sendMQTTData

--- version.h
+++ version.h
@@ -2,13 +2,13 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 735
+#define _VERSION_BUILD 737
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "04-03-2021"
-#define _VERSION_TIME "22:36:35"
+#define _VERSION_TIME "23:20:27"
 #define _SEMVER_CORE "0.8.0"
-#define _SEMVER_BUILD "0.8.0+735"
-#define _SEMVER_FULL "0.8.0+735"
+#define _SEMVER_BUILD "0.8.0+737"
+#define _SEMVER_FULL "0.8.0+737"
 #define _SEMVER_NOBUILD "0.8.0 (04-03-2021)"
-#define _VERSION "0.8.0+735 (04-03-2021)"
+#define _VERSION "0.8.0+737 (04-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
