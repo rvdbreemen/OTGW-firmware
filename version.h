fa62a3995f85825fbf7e96f660007ab46de735aa
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-06 22:26:15 +0100

    set MQTT timeout to 4 seconds

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 766
-#define _VERSION_GITHASH e3de480
+#define _VERSION_BUILD 767
+#define _VERSION_GITHASH 6935536
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "06-03-2021"
-#define _VERSION_TIME "19:10:02"
+#define _VERSION_TIME "22:21:39"
 #define _SEMVER_CORE "0.8.0"
-#define _SEMVER_BUILD "0.8.0+766"
-#define _SEMVER_GITHASH "0.8.0+e3de480"
-#define _SEMVER_FULL "0.8.0+e3de480"
+#define _SEMVER_BUILD "0.8.0+767"
+#define _SEMVER_GITHASH "0.8.0+6935536"
+#define _SEMVER_FULL "0.8.0+6935536"
 #define _SEMVER_NOBUILD "0.8.0 (06-03-2021)"
-#define _VERSION "0.8.0+e3de480 (06-03-2021)"
+#define _VERSION "0.8.0+6935536 (06-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
