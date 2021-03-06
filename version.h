b304cc6251bdf882c7335825c23acd803319c447
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-06 12:31:23 +0100

    adding githash in versioning

--- version.h
+++ version.h
@@ -2,13 +2,14 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 749
+#define _VERSION_BUILD 756
+#define _VERSION_GITHASH e1e6b3e
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "06-03-2021"
-#define _VERSION_TIME "10:54:24"
+#define _VERSION_TIME "12:29:51"
 #define _SEMVER_CORE "0.8.0"
-#define _SEMVER_BUILD "0.8.0+749"
-#define _SEMVER_FULL "0.8.0+749"
+#define _SEMVER_BUILD "0.8.0+756"
+#define _SEMVER_FULL "0.8.0+e1e6b3e-756"
 #define _SEMVER_NOBUILD "0.8.0 (06-03-2021)"
-#define _VERSION "0.8.0+749 (06-03-2021)"
+#define _VERSION "0.8.0+e1e6b3e-756 (06-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
