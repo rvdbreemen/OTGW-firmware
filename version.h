72d4ece27b9fc9f7e52bc16d87f298dd05a1d384
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-08 02:38:24 +0100

    sending mqtt uptime

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 783
-#define _VERSION_GITHASH 1775fd5
+#define _VERSION_BUILD 788
+#define _VERSION_GITHASH de9caa9
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "08-03-2021"
-#define _VERSION_TIME "02:14:32"
+#define _VERSION_TIME "02:27:17"
 #define _SEMVER_CORE "0.8.0"
-#define _SEMVER_BUILD "0.8.0+783"
-#define _SEMVER_GITHASH "0.8.0+1775fd5"
-#define _SEMVER_FULL "0.8.0+1775fd5"
+#define _SEMVER_BUILD "0.8.0+788"
+#define _SEMVER_GITHASH "0.8.0+de9caa9"
+#define _SEMVER_FULL "0.8.0+de9caa9"
 #define _SEMVER_NOBUILD "0.8.0 (08-03-2021)"
-#define _VERSION "0.8.0+1775fd5 (08-03-2021)"
+#define _VERSION "0.8.0+de9caa9 (08-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
