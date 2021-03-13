26f2199ec6f8577402455b2999549da8718be09e
sjorsjuhmaniac <sjorsjuhmaniac@localhost>
2021-03-13 10:35:18 +0100

    add new file outputs_ext.ino

---
+++ outputs_ext.ino
@@ -0,0 +1,23 @@
+/*********
+ * aaa
+ * aaa
+*********/
+
+
+// GPIO where the DS18B20 is connected to
+// Data wire is plugged TO GPIO 10
+// #define ONE_WIRE_BUS 10
+
+
+// settingGPIOOUTPUTSpin
+
+void initOutputs() {
+  if (!settingGPIOOUTPUTSenabled) return;
+
+  DebugTf("init GPIO Output on GPIO%d...\r\n", settingGPIOOUTPUTSpin);
+
+  pinMode(settingGPIOOUTPUTSpin, OUTPUT);
+
+  // set the LED with the ledState of the variable:
+  // digitalWrite(ledPin, ledState);
+}
