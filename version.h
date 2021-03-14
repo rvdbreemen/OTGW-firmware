216f865d9a96fb935afcdcfb76ede939a3ecb5be
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-14 13:58:47 +0100

    before update 0.8.1

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 802
-#define _VERSION_GITHASH de30981
+#define _VERSION_BUILD 804
+#define _VERSION_GITHASH 3c2bf8a
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "09-03-2021"
-#define _VERSION_TIME "01:00:35"
+#define _VERSION_DATE "14-03-2021"
+#define _VERSION_TIME "13:57:19"
 #define _SEMVER_CORE "0.8.1"
-#define _SEMVER_BUILD "0.8.1+802"
-#define _SEMVER_GITHASH "0.8.1+de30981"
-#define _SEMVER_FULL "0.8.1+de30981"
-#define _SEMVER_NOBUILD "0.8.1 (09-03-2021)"
-#define _VERSION "0.8.1+de30981 (09-03-2021)"
+#define _SEMVER_BUILD "0.8.1+804"
+#define _SEMVER_GITHASH "0.8.1+3c2bf8a"
+#define _SEMVER_FULL "0.8.1+3c2bf8a"
+#define _SEMVER_NOBUILD "0.8.1 (14-03-2021)"
+#define _VERSION "0.8.1+3c2bf8a (14-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
