af82ef4b28f4de95bd7c307876d87645f7c104b3
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-14 13:59:07 +0100

    update version to 0.8.1

--- data/index.js
+++ data/index.js
@@ -1,7 +1,7 @@
 /*
 ***************************************************************************  
 **  Program  : index.js, part of OTGW-firmware project
-**  Version  : v0.8.0
+**  Version  : v0.8.1
 **
 **  Copyright (c) 2021 Robert van den Breemen
 **
