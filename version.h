eb7ff52e2b2e4a0e743e1e4ad43295b7292a9aa8
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-15 22:18:30 +0100

    check littlefs for hash

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 823
-#define _VERSION_GITHASH 720b279
+#define _VERSION_BUILD 837
+#define _VERSION_GITHASH "63fabd2"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "15-03-2021"
-#define _VERSION_TIME "20:30:12"
+#define _VERSION_TIME "22:18:22"
 #define _SEMVER_CORE "0.8.2"
-#define _SEMVER_BUILD "0.8.2+823"
-#define _SEMVER_GITHASH "0.8.2+720b279"
-#define _SEMVER_FULL "0.8.2-beta+720b279"
+#define _SEMVER_BUILD "0.8.2+837"
+#define _SEMVER_GITHASH "0.8.2+63fabd2"
+#define _SEMVER_FULL "0.8.2-beta+63fabd2"
 #define _SEMVER_NOBUILD "0.8.2-beta (15-03-2021)"
-#define _VERSION "0.8.2-beta+720b279 (15-03-2021)"
+#define _VERSION "0.8.2-beta+63fabd2 (15-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
