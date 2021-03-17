bb150f749e50cd42c3dc4ef7542f8f083eecf216
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-18 00:54:25 +0100

    fixing verify fuction call

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 848
-#define _VERSION_GITHASH "ca98982"
+#define _VERSION_BUILD 849
+#define _VERSION_GITHASH "6126b44"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "18-03-2021"
-#define _VERSION_TIME "00:53:11"
+#define _VERSION_TIME "00:54:04"
 #define _SEMVER_CORE "0.8.2"
-#define _SEMVER_BUILD "0.8.2+848"
-#define _SEMVER_GITHASH "0.8.2+ca98982"
-#define _SEMVER_FULL "0.8.2-beta+ca98982"
+#define _SEMVER_BUILD "0.8.2+849"
+#define _SEMVER_GITHASH "0.8.2+6126b44"
+#define _SEMVER_FULL "0.8.2-beta+6126b44"
 #define _SEMVER_NOBUILD "0.8.2-beta (18-03-2021)"
-#define _VERSION "0.8.2-beta+ca98982 (18-03-2021)"
+#define _VERSION "0.8.2-beta+6126b44 (18-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
