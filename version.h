2c2ab63939af9df5554d01731d551ec686d41e12
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-20 00:48:39 +0100

    hooking mqtt command to queue code

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 855
-#define _VERSION_GITHASH "8f83795"
+#define _VERSION_BUILD 858
+#define _VERSION_GITHASH "95eda50"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "19-03-2021"
-#define _VERSION_TIME "23:42:06"
+#define _VERSION_DATE "20-03-2021"
+#define _VERSION_TIME "00:47:21"
 #define _SEMVER_CORE "0.8.2"
-#define _SEMVER_BUILD "0.8.2+855"
-#define _SEMVER_GITHASH "0.8.2+8f83795"
-#define _SEMVER_FULL "0.8.2-beta+8f83795"
-#define _SEMVER_NOBUILD "0.8.2-beta (19-03-2021)"
-#define _VERSION "0.8.2-beta+8f83795 (19-03-2021)"
+#define _SEMVER_BUILD "0.8.2+858"
+#define _SEMVER_GITHASH "0.8.2+95eda50"
+#define _SEMVER_FULL "0.8.2-beta+95eda50"
+#define _SEMVER_NOBUILD "0.8.2-beta (20-03-2021)"
+#define _VERSION "0.8.2-beta+95eda50 (20-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
