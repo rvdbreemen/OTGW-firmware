7226e6bbf08155e79aed797d28a5c806c4512815
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-20 01:10:19 +0100

    correct handeling of expired queue entries

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 858
-#define _VERSION_GITHASH "95eda50"
+#define _VERSION_BUILD 862
+#define _VERSION_GITHASH "2c2ab63"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "20-03-2021"
-#define _VERSION_TIME "00:47:21"
+#define _VERSION_TIME "01:09:47"
 #define _SEMVER_CORE "0.8.2"
-#define _SEMVER_BUILD "0.8.2+858"
-#define _SEMVER_GITHASH "0.8.2+95eda50"
-#define _SEMVER_FULL "0.8.2-beta+95eda50"
+#define _SEMVER_BUILD "0.8.2+862"
+#define _SEMVER_GITHASH "0.8.2+2c2ab63"
+#define _SEMVER_FULL "0.8.2-beta+2c2ab63"
 #define _SEMVER_NOBUILD "0.8.2-beta (20-03-2021)"
-#define _VERSION "0.8.2-beta+95eda50 (20-03-2021)"
+#define _VERSION "0.8.2-beta+2c2ab63 (20-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
