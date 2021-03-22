dbb35a8c4e8d5fa17f2952c034506e5f8de45593
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-22 20:23:47 +0100

    fixing bits in status lower masks

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 807
-#define _VERSION_GITHASH 92f10cb
+#define _VERSION_BUILD 808
+#define _VERSION_GITHASH "f824ea0"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "14-03-2021"
-#define _VERSION_TIME "14:56:09"
+#define _VERSION_DATE "22-03-2021"
+#define _VERSION_TIME "20:22:52"
 #define _SEMVER_CORE "0.8.1"
-#define _SEMVER_BUILD "0.8.1+807"
-#define _SEMVER_GITHASH "0.8.1+92f10cb"
-#define _SEMVER_FULL "0.8.1+92f10cb"
-#define _SEMVER_NOBUILD "0.8.1 (14-03-2021)"
-#define _VERSION "0.8.1+92f10cb (14-03-2021)"
+#define _SEMVER_BUILD "0.8.1+808"
+#define _SEMVER_GITHASH "0.8.1+f824ea0"
+#define _SEMVER_FULL "0.8.1+f824ea0"
+#define _SEMVER_NOBUILD "0.8.1 (22-03-2021)"
+#define _VERSION "0.8.1+f824ea0 (22-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
