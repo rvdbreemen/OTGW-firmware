a6f4907511c1559943524527a080782dc9ddc4c6
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-22 20:21:22 +0100

    fixing queues

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 862
-#define _VERSION_GITHASH "2c2ab63"
+#define _VERSION_BUILD 882
+#define _VERSION_GITHASH "b296972"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "20-03-2021"
-#define _VERSION_TIME "01:09:47"
+#define _VERSION_DATE "22-03-2021"
+#define _VERSION_TIME "20:20:31"
 #define _SEMVER_CORE "0.8.2"
-#define _SEMVER_BUILD "0.8.2+862"
-#define _SEMVER_GITHASH "0.8.2+2c2ab63"
-#define _SEMVER_FULL "0.8.2-beta+2c2ab63"
-#define _SEMVER_NOBUILD "0.8.2-beta (20-03-2021)"
-#define _VERSION "0.8.2-beta+2c2ab63 (20-03-2021)"
+#define _SEMVER_BUILD "0.8.2+882"
+#define _SEMVER_GITHASH "0.8.2+b296972"
+#define _SEMVER_FULL "0.8.2-beta+b296972"
+#define _SEMVER_NOBUILD "0.8.2-beta (22-03-2021)"
+#define _VERSION "0.8.2-beta+b296972 (22-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
