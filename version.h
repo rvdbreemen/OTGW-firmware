9260208c371987e4e0e029184366bcb104e7314e
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-23 22:28:07 +0100

    only move queue if not last entry

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 882
-#define _VERSION_GITHASH "b296972"
+#define _VERSION_BUILD 885
+#define _VERSION_GITHASH "721fa1c"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "22-03-2021"
-#define _VERSION_TIME "20:20:31"
+#define _VERSION_DATE "23-03-2021"
+#define _VERSION_TIME "22:26:24"
 #define _SEMVER_CORE "0.8.2"
-#define _SEMVER_BUILD "0.8.2+882"
-#define _SEMVER_GITHASH "0.8.2+b296972"
-#define _SEMVER_FULL "0.8.2-beta+b296972"
-#define _SEMVER_NOBUILD "0.8.2-beta (22-03-2021)"
-#define _VERSION "0.8.2-beta+b296972 (22-03-2021)"
+#define _SEMVER_BUILD "0.8.2+885"
+#define _SEMVER_GITHASH "0.8.2+721fa1c"
+#define _SEMVER_FULL "0.8.2-beta+721fa1c"
+#define _SEMVER_NOBUILD "0.8.2-beta (23-03-2021)"
+#define _VERSION "0.8.2-beta+721fa1c (23-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
