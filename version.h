caa806388a18ae768ee189b5e70b4941fc09346f
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-23 23:51:55 +0100

    fixing passing string

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 890
-#define _VERSION_GITHASH "24598dd"
+#define _VERSION_BUILD 894
+#define _VERSION_GITHASH "b85d40b"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "23-03-2021"
-#define _VERSION_TIME "23:35:58"
+#define _VERSION_TIME "23:49:31"
 #define _SEMVER_CORE "0.8.2"
-#define _SEMVER_BUILD "0.8.2+890"
-#define _SEMVER_GITHASH "0.8.2+24598dd"
-#define _SEMVER_FULL "0.8.2-beta+24598dd"
+#define _SEMVER_BUILD "0.8.2+894"
+#define _SEMVER_GITHASH "0.8.2+b85d40b"
+#define _SEMVER_FULL "0.8.2-beta+b85d40b"
 #define _SEMVER_NOBUILD "0.8.2-beta (23-03-2021)"
-#define _VERSION "0.8.2-beta+24598dd (23-03-2021)"
+#define _VERSION "0.8.2-beta+b85d40b (23-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
