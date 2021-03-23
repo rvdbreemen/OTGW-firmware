a34784ca2523268c6bfc32b65b5c04760baeafca
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-24 00:03:06 +0100

    fixing settings ui

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 895
-#define _VERSION_GITHASH "caa8063"
+#define _VERSION_BUILD 898
+#define _VERSION_GITHASH "9d3feab"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "23-03-2021"
-#define _VERSION_TIME "23:53:32"
+#define _VERSION_DATE "24-03-2021"
+#define _VERSION_TIME "00:02:46"
 #define _SEMVER_CORE "0.8.2"
-#define _SEMVER_BUILD "0.8.2+895"
-#define _SEMVER_GITHASH "0.8.2+caa8063"
-#define _SEMVER_FULL "0.8.2-beta+caa8063"
-#define _SEMVER_NOBUILD "0.8.2-beta (23-03-2021)"
-#define _VERSION "0.8.2-beta+caa8063 (23-03-2021)"
+#define _SEMVER_BUILD "0.8.2+898"
+#define _SEMVER_GITHASH "0.8.2+9d3feab"
+#define _SEMVER_FULL "0.8.2-beta+9d3feab"
+#define _SEMVER_NOBUILD "0.8.2-beta (24-03-2021)"
+#define _VERSION "0.8.2-beta+9d3feab (24-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
