620acaa9137cab1032446e4780f7dc762d0535e9
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-23 21:47:36 +0100

    pass-by-reference bug fixed

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 808
-#define _VERSION_GITHASH "f824ea0"
+#define _VERSION_BUILD 855
+#define _VERSION_GITHASH "dbb35a8"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "22-03-2021"
-#define _VERSION_TIME "20:22:52"
+#define _VERSION_DATE "23-03-2021"
+#define _VERSION_TIME "21:46:16"
 #define _SEMVER_CORE "0.8.1"
-#define _SEMVER_BUILD "0.8.1+808"
-#define _SEMVER_GITHASH "0.8.1+f824ea0"
-#define _SEMVER_FULL "0.8.1+f824ea0"
-#define _SEMVER_NOBUILD "0.8.1 (22-03-2021)"
-#define _VERSION "0.8.1+f824ea0 (22-03-2021)"
+#define _SEMVER_BUILD "0.8.1+855"
+#define _SEMVER_GITHASH "0.8.1+dbb35a8"
+#define _SEMVER_FULL "0.8.1+dbb35a8"
+#define _SEMVER_NOBUILD "0.8.1 (23-03-2021)"
+#define _VERSION "0.8.1+dbb35a8 (23-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
