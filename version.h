1802f4954c1e1704efc3610b2b3ba8280bfa7ff6
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-24 21:23:22 +0100

    fixing bug in assiging the value

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 920
-#define _VERSION_GITHASH "a34784c"
+#define _VERSION_BUILD 921
+#define _VERSION_GITHASH "8bcd31c"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "24-03-2021"
-#define _VERSION_TIME "19:42:29"
+#define _VERSION_TIME "21:16:05"
 #define _SEMVER_CORE "0.8.2"
-#define _SEMVER_BUILD "0.8.2+920"
-#define _SEMVER_GITHASH "0.8.2+a34784c"
-#define _SEMVER_FULL "0.8.2-beta+a34784c"
+#define _SEMVER_BUILD "0.8.2+921"
+#define _SEMVER_GITHASH "0.8.2+8bcd31c"
+#define _SEMVER_FULL "0.8.2-beta+8bcd31c"
 #define _SEMVER_NOBUILD "0.8.2-beta (24-03-2021)"
-#define _VERSION "0.8.2-beta+a34784c (24-03-2021)"
+#define _VERSION "0.8.2-beta+8bcd31c (24-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
