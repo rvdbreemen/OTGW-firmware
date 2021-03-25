4a9045fc01056640d7cd7cde88de1be928238b58
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-25 21:40:44 +0100

    update littlefs hash

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 923
-#define _VERSION_GITHASH "d1a2cc0"
-// #define _VERSION_PRERELEASE beta
+#define _VERSION_BUILD 924
+#define _VERSION_GITHASH "095b077"
+//#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "25-03-2021"
-#define _VERSION_TIME "21:24:09"
+#define _VERSION_TIME "21:32:25"
 #define _SEMVER_CORE "0.8.2"
-#define _SEMVER_BUILD "0.8.2+923"
-#define _SEMVER_GITHASH "0.8.2+d1a2cc0"
-#define _SEMVER_FULL "0.8.2-beta+d1a2cc0"
-#define _SEMVER_NOBUILD "0.8.2-beta (25-03-2021)"
-#define _VERSION "0.8.2-beta+d1a2cc0 (25-03-2021)"
+#define _SEMVER_BUILD "0.8.2+924"
+#define _SEMVER_GITHASH "0.8.2+095b077"
+#define _SEMVER_FULL "0.8.2+095b077"
+#define _SEMVER_NOBUILD "0.8.2 (25-03-2021)"
+#define _VERSION "0.8.2+095b077 (25-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
