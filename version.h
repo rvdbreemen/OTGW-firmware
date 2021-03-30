c4282140e0c5c700e41d5f89ae5c929c5690874e
Robert van den Breemen <robert@vandenbreemen.net>
2021-03-30 21:33:37 +0200

    Fixing debug verbose in MQTT

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 932
-#define _VERSION_GITHASH "60d2216"
+#define _VERSION_BUILD 934
+#define _VERSION_GITHASH "1da0800"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "28-03-2021"
-#define _VERSION_TIME "18:50:55"
+#define _VERSION_DATE "30-03-2021"
+#define _VERSION_TIME "21:23:11"
 #define _SEMVER_CORE "0.8.2"
-#define _SEMVER_BUILD "0.8.2+932"
-#define _SEMVER_GITHASH "0.8.2+60d2216"
-#define _SEMVER_FULL "0.8.2+60d2216"
-#define _SEMVER_NOBUILD "0.8.2 (28-03-2021)"
-#define _VERSION "0.8.2+60d2216 (28-03-2021)"
+#define _SEMVER_BUILD "0.8.2+934"
+#define _SEMVER_GITHASH "0.8.2+1da0800"
+#define _SEMVER_FULL "0.8.2+1da0800"
+#define _SEMVER_NOBUILD "0.8.2 (30-03-2021)"
+#define _VERSION "0.8.2+1da0800 (30-03-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
