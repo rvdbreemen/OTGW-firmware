bdef4732816eb5acf2342c260bde0b8df08f7be6
Robert van den Breemen <robert@vandenbreemen.net>
2021-04-01 22:29:54 +0200

    Adding debug help menu

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 961
-#define _VERSION_GITHASH "859df0d"
+#define _VERSION_BUILD 962
+#define _VERSION_GITHASH "98d6f7b"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "30-03-2021"
-#define _VERSION_TIME "23:37:05"
+#define _VERSION_DATE "01-04-2021"
+#define _VERSION_TIME "22:25:53"
 #define _SEMVER_CORE "0.8.3"
-#define _SEMVER_BUILD "0.8.3+961"
-#define _SEMVER_GITHASH "0.8.3+859df0d"
-#define _SEMVER_FULL "0.8.3-beta+859df0d"
-#define _SEMVER_NOBUILD "0.8.3-beta (30-03-2021)"
-#define _VERSION "0.8.3-beta+859df0d (30-03-2021)"
+#define _SEMVER_BUILD "0.8.3+962"
+#define _SEMVER_GITHASH "0.8.3+98d6f7b"
+#define _SEMVER_FULL "0.8.3-beta+98d6f7b"
+#define _SEMVER_NOBUILD "0.8.3-beta (01-04-2021)"
+#define _VERSION "0.8.3-beta+98d6f7b (01-04-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
