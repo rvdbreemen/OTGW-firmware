d0b72bdf693ae702633977522fa30399fe20a13d
Robert van den Breemen <robert@vandenbreemen.net>
2021-04-03 23:11:50 +0200

    versision detectoin changed.

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 970
-#define _VERSION_GITHASH "f65323d"
+#define _VERSION_BUILD 973
+#define _VERSION_GITHASH "7fa7264"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "03-04-2021"
-#define _VERSION_TIME "18:13:54"
+#define _VERSION_TIME "19:53:36"
 #define _SEMVER_CORE "0.8.3"
-#define _SEMVER_BUILD "0.8.3+970"
-#define _SEMVER_GITHASH "0.8.3+f65323d"
-#define _SEMVER_FULL "0.8.3-beta+f65323d"
+#define _SEMVER_BUILD "0.8.3+973"
+#define _SEMVER_GITHASH "0.8.3+7fa7264"
+#define _SEMVER_FULL "0.8.3-beta+7fa7264"
 #define _SEMVER_NOBUILD "0.8.3-beta (03-04-2021)"
-#define _VERSION "0.8.3-beta+f65323d (03-04-2021)"
+#define _VERSION "0.8.3-beta+7fa7264 (03-04-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
