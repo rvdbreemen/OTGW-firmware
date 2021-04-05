e4a0e2e2b3b714188f90c7e9c557a2d3c9855452
Robert van den Breemen <robert@vandenbreemen.net>
2021-04-05 21:43:49 +0200

    Removed some whitepace

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 976
-#define _VERSION_GITHASH "6031d2f"
+#define _VERSION_BUILD 979
+#define _VERSION_GITHASH "2efb048"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "05-04-2021"
-#define _VERSION_TIME "00:21:00"
+#define _VERSION_TIME "21:43:27"
 #define _SEMVER_CORE "0.8.3"
-#define _SEMVER_BUILD "0.8.3+976"
-#define _SEMVER_GITHASH "0.8.3+6031d2f"
-#define _SEMVER_FULL "0.8.3-beta+6031d2f"
+#define _SEMVER_BUILD "0.8.3+979"
+#define _SEMVER_GITHASH "0.8.3+2efb048"
+#define _SEMVER_FULL "0.8.3-beta+2efb048"
 #define _SEMVER_NOBUILD "0.8.3-beta (05-04-2021)"
-#define _VERSION "0.8.3-beta+6031d2f (05-04-2021)"
+#define _VERSION "0.8.3-beta+2efb048 (05-04-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
