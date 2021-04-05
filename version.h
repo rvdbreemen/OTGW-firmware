2fa1f1f97de6d04efbceb62be5851d40d7fad0e4
Robert van den Breemen <robert@vandenbreemen.net>
2021-04-05 21:54:23 +0200

    update version.hash to 1b8259b

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 979
-#define _VERSION_GITHASH "2efb048"
-//#define _VERSION_PRERELEASE beta
+#define _VERSION_BUILD 980
+#define _VERSION_GITHASH "1b8259b"
+//#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "05-04-2021"
-#define _VERSION_TIME "21:43:27"
+#define _VERSION_TIME "21:51:11"
 #define _SEMVER_CORE "0.8.3"
-#define _SEMVER_BUILD "0.8.3+979"
-#define _SEMVER_GITHASH "0.8.3+2efb048"
-#define _SEMVER_FULL "0.8.3-beta+2efb048"
-#define _SEMVER_NOBUILD "0.8.3-beta (05-04-2021)"
-#define _VERSION "0.8.3-beta+2efb048 (05-04-2021)"
+#define _SEMVER_BUILD "0.8.3+980"
+#define _SEMVER_GITHASH "0.8.3+1b8259b"
+#define _SEMVER_FULL "0.8.3+1b8259b"
+#define _SEMVER_NOBUILD "0.8.3 (05-04-2021)"
+#define _VERSION "0.8.3+1b8259b (05-04-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
