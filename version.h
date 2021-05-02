2b36e3a175fd44cd8044ccd98f880223288d86dd
Robert van den Breemen <robert@vandenbreemen.net>
2021-05-02 18:43:17 +0200

    Debug OTmessage parsing always prints received message now
    Parsing status bits for master and slave only from boiler message
    Fixed debug otmessage output

--- version.h
+++ version.h
@@ -1,16 +1,16 @@
 //The version number conforms to semver.org format
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
-#define _VERSION_PATCH 4
-#define _VERSION_BUILD 997
-#define _VERSION_GITHASH "f39b68a"
-//#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "02-05-2021"
-#define _VERSION_TIME "17:52:56"
-#define _SEMVER_CORE "0.8.4"
-#define _SEMVER_BUILD "0.8.4+997"
-#define _SEMVER_GITHASH "0.8.4+f39b68a"
-#define _SEMVER_FULL "0.8.4+f39b68a"
-#define _SEMVER_NOBUILD "0.8.4 (02-05-2021)"
-#define _VERSION "0.8.4+f39b68a (02-05-2021)"
+#define _VERSION_PATCH 5
+#define _VERSION_BUILD 1016
+#define _VERSION_GITHASH "0f07d02"
+#define _VERSION_PRERELEASE beta
+#define _VERSION_DATE "26-06-2021"
+#define _VERSION_TIME "14:11:00"
+#define _SEMVER_CORE "0.8.5"
+#define _SEMVER_BUILD "0.8.5+1016"
+#define _SEMVER_GITHASH "0.8.5+0f07d02"
+#define _SEMVER_FULL "0.8.5-beta+0f07d02"
+#define _SEMVER_NOBUILD "0.8.5-beta (26-06-2021)"
+#define _VERSION "0.8.5-beta+0f07d02 (26-06-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
