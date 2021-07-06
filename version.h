852a75e3abe3b6658ed41f124e0efc3aa2725e1d
Robert van den Breemen <robert@vandenbreemen.net>
2021-07-07 00:12:51 +0200

    fix a mqtt homeassistant configuration issue

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 5
-#define _VERSION_BUILD 1027
-#define _VERSION_GITHASH "53d8b3e"
+#define _VERSION_BUILD 1028
+#define _VERSION_GITHASH "dc3dc14"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "30-06-2021"
-#define _VERSION_TIME "21:54:49"
+#define _VERSION_DATE "01-07-2021"
+#define _VERSION_TIME "21:35:18"
 #define _SEMVER_CORE "0.8.5"
-#define _SEMVER_BUILD "0.8.5+1027"
-#define _SEMVER_GITHASH "0.8.5+53d8b3e"
-#define _SEMVER_FULL "0.8.5+53d8b3e"
-#define _SEMVER_NOBUILD "0.8.5 (30-06-2021)"
-#define _VERSION "0.8.5+53d8b3e (30-06-2021)"
+#define _SEMVER_BUILD "0.8.5+1028"
+#define _SEMVER_GITHASH "0.8.5+dc3dc14"
+#define _SEMVER_FULL "0.8.5+dc3dc14"
+#define _SEMVER_NOBUILD "0.8.5 (01-07-2021)"
+#define _VERSION "0.8.5+dc3dc14 (01-07-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
