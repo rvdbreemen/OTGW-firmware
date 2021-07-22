24d8d5a45f5b4e78aafea3d5866cd3ae13c2c66a
Robert van den Breemen <robert@vandenbreemen.net>
2021-07-22 21:51:28 +0200

    Addeing OEMDiagnosticCode topic for auto discovery

--- version.h
+++ version.h
@@ -1,16 +1,16 @@
 //The version number conforms to semver.org format
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
-#define _VERSION_PATCH 5
-#define _VERSION_BUILD 1028
-#define _VERSION_GITHASH "dc3dc14"
-//#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "01-07-2021"
-#define _VERSION_TIME "21:35:18"
-#define _SEMVER_CORE "0.8.5"
-#define _SEMVER_BUILD "0.8.5+1028"
-#define _SEMVER_GITHASH "0.8.5+dc3dc14"
-#define _SEMVER_FULL "0.8.5+dc3dc14"
-#define _SEMVER_NOBUILD "0.8.5 (01-07-2021)"
-#define _VERSION "0.8.5+dc3dc14 (01-07-2021)"
+#define _VERSION_PATCH 6
+#define _VERSION_BUILD 1031
+#define _VERSION_GITHASH "9401a06"
+#define _VERSION_PRERELEASE beta
+#define _VERSION_DATE "22-07-2021"
+#define _VERSION_TIME "21:51:01"
+#define _SEMVER_CORE "0.8.6"
+#define _SEMVER_BUILD "0.8.6+1031"
+#define _SEMVER_GITHASH "0.8.6+9401a06"
+#define _SEMVER_FULL "0.8.6-beta+9401a06"
+#define _SEMVER_NOBUILD "0.8.6-beta (22-07-2021)"
+#define _VERSION "0.8.6-beta+9401a06 (22-07-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
