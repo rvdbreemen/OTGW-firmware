e8f2b1b0231747235ef232c624dc9ed25f2db289
Robert van den Breemen <robert@vandenbreemen.net>
2021-07-25 21:10:05 +0200

    Fixed readme, removed old "command topic"

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 5
-#define _VERSION_BUILD 1028
-#define _VERSION_GITHASH "dc3dc14"
+#define _VERSION_BUILD 1029
+#define _VERSION_GITHASH "9401a06"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "01-07-2021"
-#define _VERSION_TIME "21:35:18"
+#define _VERSION_DATE "25-07-2021"
+#define _VERSION_TIME "21:06:48"
 #define _SEMVER_CORE "0.8.5"
-#define _SEMVER_BUILD "0.8.5+1028"
-#define _SEMVER_GITHASH "0.8.5+dc3dc14"
-#define _SEMVER_FULL "0.8.5+dc3dc14"
-#define _SEMVER_NOBUILD "0.8.5 (01-07-2021)"
-#define _VERSION "0.8.5+dc3dc14 (01-07-2021)"
+#define _SEMVER_BUILD "0.8.5+1029"
+#define _SEMVER_GITHASH "0.8.5+9401a06"
+#define _SEMVER_FULL "0.8.5+9401a06"
+#define _SEMVER_NOBUILD "0.8.5 (25-07-2021)"
+#define _VERSION "0.8.5+9401a06 (25-07-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
