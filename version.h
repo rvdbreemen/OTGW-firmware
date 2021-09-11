e5b0242bc81730d3b6c97efb472040ab4de20e84
Robert van den Breemen <robert@vandenbreemen.net>
2021-09-11 17:46:11 +0200

    Adding autoconfig rest api endpoint

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 6
-#define _VERSION_BUILD 1046
-#define _VERSION_GITHASH "de13af2"
+#define _VERSION_BUILD 1049
+#define _VERSION_GITHASH "3656042"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "10-09-2021"
-#define _VERSION_TIME "01:29:41"
+#define _VERSION_TIME "14:07:20"
 #define _SEMVER_CORE "0.8.6"
-#define _SEMVER_BUILD "0.8.6+1046"
-#define _SEMVER_GITHASH "0.8.6+de13af2"
-#define _SEMVER_FULL "0.8.6-beta+de13af2"
+#define _SEMVER_BUILD "0.8.6+1049"
+#define _SEMVER_GITHASH "0.8.6+3656042"
+#define _SEMVER_FULL "0.8.6-beta+3656042"
 #define _SEMVER_NOBUILD "0.8.6-beta (10-09-2021)"
-#define _VERSION "0.8.6-beta+de13af2 (10-09-2021)"
+#define _VERSION "0.8.6-beta+3656042 (10-09-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
