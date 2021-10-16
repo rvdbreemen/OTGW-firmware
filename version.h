45b51f2068e1801949e3c3a6014ab9ed77146a5a
Robert van den Breemen <robert@vandenbreemen.net>
2021-10-16 20:36:00 +0200

    using configTime and AceTime to replace ezTime

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 7
-#define _VERSION_BUILD 1089
-#define _VERSION_GITHASH "9c79ca5"
+#define _VERSION_BUILD 1119
+#define _VERSION_GITHASH "a449a01"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "16-09-2021"
-#define _VERSION_TIME "23:50:25"
+#define _VERSION_DATE "16-10-2021"
+#define _VERSION_TIME "19:03:17"
 #define _SEMVER_CORE "0.8.7"
-#define _SEMVER_BUILD "0.8.7+1089"
-#define _SEMVER_GITHASH "0.8.7+9c79ca5"
-#define _SEMVER_FULL "0.8.7-beta+9c79ca5"
-#define _SEMVER_NOBUILD "0.8.7-beta (16-09-2021)"
-#define _VERSION "0.8.7-beta+9c79ca5 (16-09-2021)"
+#define _SEMVER_BUILD "0.8.7+1119"
+#define _SEMVER_GITHASH "0.8.7+a449a01"
+#define _SEMVER_FULL "0.8.7-beta+a449a01"
+#define _SEMVER_NOBUILD "0.8.7-beta (16-10-2021)"
+#define _VERSION "0.8.7-beta+a449a01 (16-10-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
