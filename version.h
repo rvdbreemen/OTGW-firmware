99dd7178bcccdba04af6f5f2d10edfc7b54234b1
Robert van den Breemen <robert@vandenbreemen.net>
2021-10-16 21:46:10 +0200

    Fallback to default if error looking up TZ

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
 #define _VERSION_PATCH 7
-#define _VERSION_BUILD 1119
-#define _VERSION_GITHASH "a449a01"
+#define _VERSION_BUILD 1120
+#define _VERSION_GITHASH "45b51f2"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "16-10-2021"
-#define _VERSION_TIME "19:03:17"
+#define _VERSION_TIME "21:12:55"
 #define _SEMVER_CORE "0.8.7"
-#define _SEMVER_BUILD "0.8.7+1119"
-#define _SEMVER_GITHASH "0.8.7+a449a01"
-#define _SEMVER_FULL "0.8.7-beta+a449a01"
+#define _SEMVER_BUILD "0.8.7+1120"
+#define _SEMVER_GITHASH "0.8.7+45b51f2"
+#define _SEMVER_FULL "0.8.7-beta+45b51f2"
 #define _SEMVER_NOBUILD "0.8.7-beta (16-10-2021)"
-#define _VERSION "0.8.7-beta+a449a01 (16-10-2021)"
+#define _VERSION "0.8.7-beta+45b51f2 (16-10-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
