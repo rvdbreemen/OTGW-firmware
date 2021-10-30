06a79e0a3b7632ae246018b7444a97b6876f9381
Robert van den Breemen <robert@vandenbreemen.net>
2021-10-30 17:50:01 +0200

    trying to resolve merge

--- version.h
+++ version.h
@@ -1,16 +1,16 @@
 //The version number conforms to semver.org format
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 8  
-#define _VERSION_PATCH 6
-#define _VERSION_BUILD 1052
-#define _VERSION_GITHASH "14e2267"
-//#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "12-09-2021"
-#define _VERSION_TIME "19:58:14"
-#define _SEMVER_CORE "0.8.6"
-#define _SEMVER_BUILD "0.8.6+1052"
-#define _SEMVER_GITHASH "0.8.6+14e2267"
-#define _SEMVER_FULL "0.8.6+14e2267"
-#define _SEMVER_NOBUILD "0.8.6 (12-09-2021)"
-#define _VERSION "0.8.6+14e2267 (12-09-2021)"
+#define _VERSION_PATCH 7
+#define _VERSION_BUILD 1167
+#define _VERSION_GITHASH "736eca8"
+#define _VERSION_PRERELEASE beta
+#define _VERSION_DATE "30-10-2021"
+#define _VERSION_TIME "17:48:05"
+#define _SEMVER_CORE "0.8.7"
+#define _SEMVER_BUILD "0.8.7+1167"
+#define _SEMVER_GITHASH "0.8.7+736eca8"
+#define _SEMVER_FULL "0.8.7-beta+736eca8"
+#define _SEMVER_NOBUILD "0.8.7-beta (30-10-2021)"
+#define _VERSION "0.8.7-beta+736eca8 (30-10-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
