9c80ddb232b1e7a77a0e6320168cec66fcd1faa6
Robert van den Breemen <robert@vandenbreemen.net>
2021-11-03 19:01:47 +0100

    update version to 0.9.0-beta

--- FSexplorer.ino
+++ FSexplorer.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program : FSexplorer
-**  Version  : v0.8.6
+**  Version  : v0.9.0-beta
 **
 **  Mostly stolen from https://www.arduinoforum.de/User-Fips
 **  For more information visit: https://fipsok.de
