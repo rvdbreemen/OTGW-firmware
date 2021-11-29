22ff900c186281886e4f5fc1a800eea037675a43
Robert van den Breemen <robert@vandenbreemen.net>
2021-11-29 23:18:31 +0100

    No complaining when no internet or version retrieved from PIC

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 1224
-#define _VERSION_GITHASH "8e7bdcd"
+#define _VERSION_BUILD 1228
+#define _VERSION_GITHASH "6ecf98f"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "16-11-2021"
-#define _VERSION_TIME "23:49:34"
+#define _VERSION_DATE "29-11-2021"
+#define _VERSION_TIME "23:12:35"
 #define _SEMVER_CORE "0.9.1"
-#define _SEMVER_BUILD "0.9.1+1224"
-#define _SEMVER_GITHASH "0.9.1+8e7bdcd"
-#define _SEMVER_FULL "0.9.1-beta+8e7bdcd"
-#define _SEMVER_NOBUILD "0.9.1-beta (16-11-2021)"
-#define _VERSION "0.9.1-beta+8e7bdcd (16-11-2021)"
+#define _SEMVER_BUILD "0.9.1+1228"
+#define _SEMVER_GITHASH "0.9.1+6ecf98f"
+#define _SEMVER_FULL "0.9.1-beta+6ecf98f"
+#define _SEMVER_NOBUILD "0.9.1-beta (29-11-2021)"
+#define _VERSION "0.9.1-beta+6ecf98f (29-11-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
