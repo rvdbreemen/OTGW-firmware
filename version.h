42c1792f9d5ec96e4ac2c453ed24649c6bc452f1
Robert van den Breemen <robert@vandenbreemen.net>
2021-12-04 00:40:15 +0100

    surpress all DATA READ to MQTT

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 1269
-#define _VERSION_GITHASH "4e86c38"
+#define _VERSION_BUILD 1275
+#define _VERSION_GITHASH "ea66142"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "03-12-2021"
-#define _VERSION_TIME "23:00:15"
+#define _VERSION_DATE "04-12-2021"
+#define _VERSION_TIME "00:37:03"
 #define _SEMVER_CORE "0.9.1"
-#define _SEMVER_BUILD "0.9.1+1269"
-#define _SEMVER_GITHASH "0.9.1+4e86c38"
-#define _SEMVER_FULL "0.9.1-beta+4e86c38"
-#define _SEMVER_NOBUILD "0.9.1-beta (03-12-2021)"
-#define _VERSION "0.9.1-beta+4e86c38 (03-12-2021)"
+#define _SEMVER_BUILD "0.9.1+1275"
+#define _SEMVER_GITHASH "0.9.1+ea66142"
+#define _SEMVER_FULL "0.9.1-beta+ea66142"
+#define _SEMVER_NOBUILD "0.9.1-beta (04-12-2021)"
+#define _VERSION "0.9.1-beta+ea66142 (04-12-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
