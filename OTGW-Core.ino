e460e49595ba00bd89e8331fe4fa68b8223a4f0b
Robert van den Breemen <robert@vandenbreemen.net>
2021-12-05 23:01:49 +0100

    Fix to make it run on Arduino 3.0.2
    fixing sendOTGW and updating AceTime & AceSorting
    
    Thanks to RobR the upgrade to SDK 3.0.2 is possible, because of the fix of sendOTGW
    
    removing .DS
    
    remove .DS

--- OTGW-Core.ino
+++ OTGW-Core.ino
@@ -1180,8 +1180,13 @@ void checkOTGWcmdqueue(const char *buf, int len){
   sendOTGW(const char* buf, int len) sends a string to the serial OTGW device.
   The buffer is send out to OTGW on the serial device instantly, as long as there is space in the buffer.
 */
-int sendOTGW(const char* buf, int len)
+void sendOTGW(const char* buf, int len)
 {
+  // while (OTGWSerial.availableForWrite() < (len+2)) {
+  //   //cannot write, buffer full, wait for some space in serial out buffer
+  // feedWatchDog();     //this yields for other processes
+  // }
+
   //Send the buffer to OTGW when the Serial interface is available
   if (OTGWSerial.availableForWrite()>=len+2) {
     //check the write buffer
@@ -1191,21 +1196,13 @@ int sendOTGW(const char* buf, int len)
       OTGWDebug((char)buf[i]);
     }
     OTGWDebug("] ("); OTGWDebug(len); OTGWDebug(")"); OTGWDebugln();
-    
-    while (OTGWSerial.availableForWrite()==(len+2)) {
-      //cannot write, buffer full, wait for some space in serial out buffer
-      feedWatchDog();     //this yields for other processes
-    }
-
-    if (OTGWSerial.availableForWrite()>= (len+2)) {
-      //write buffer to serial
-      OTGWSerial.write(buf, len);
-      // OTGWSerial.write("PS=0\r\n");
-      OTGWSerial.write('\r');
-      OTGWSerial.write('\n');
-      OTGWSerial.flush(); 
-    } else OTGWDebugln("Error: Write buffer not big enough!");
-  } else OTGWDebugln("Error: Serial device not found!");
+        
+    //write buffer to serial
+    OTGWSerial.write(buf, len);         
+    OTGWSerial.write('\r');
+    OTGWSerial.write('\n');            
+    OTGWSerial.flush(); 
+  } else OTGWDebugln("Error: Write buffer not big enough!");
 }
 
 /*
