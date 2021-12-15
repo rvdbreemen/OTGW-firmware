f6a05cc772818925d0d704e8d276d4ccb147a7e3
Robert van den Breemen <robert@vandenbreemen.net>
2021-12-15 11:53:11 +0100

    adding the msg id to evaluation of skip criteria
    Only skip a message if it overridden by the same msg id.

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 1320
-#define _VERSION_GITHASH "3ca8c24"
+#define _VERSION_BUILD 1334
+#define _VERSION_GITHASH "c38d49a"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "15-12-2021"
-#define _VERSION_TIME "09:40:46"
+#define _VERSION_TIME "11:49:42"
 #define _SEMVER_CORE "0.9.1"
-#define _SEMVER_BUILD "0.9.1+1320"
-#define _SEMVER_GITHASH "0.9.1+3ca8c24"
-#define _SEMVER_FULL "0.9.1-beta+3ca8c24"
+#define _SEMVER_BUILD "0.9.1+1334"
+#define _SEMVER_GITHASH "0.9.1+c38d49a"
+#define _SEMVER_FULL "0.9.1-beta+c38d49a"
 #define _SEMVER_NOBUILD "0.9.1-beta (15-12-2021)"
-#define _VERSION "0.9.1-beta+3ca8c24 (15-12-2021)"
+#define _VERSION "0.9.1-beta+c38d49a (15-12-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
