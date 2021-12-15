aebf19022a7f7e7219c244d4bb6330c35639da0d
Robert van den Breemen <robert@vandenbreemen.net>
2021-12-15 09:43:57 +0100

    fixing typo in bootcmds refactor

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 1317
-#define _VERSION_GITHASH "7f41cd0"
+#define _VERSION_BUILD 1320
+#define _VERSION_GITHASH "3ca8c24"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "12-12-2021"
-#define _VERSION_TIME "20:00:31"
+#define _VERSION_DATE "15-12-2021"
+#define _VERSION_TIME "09:40:46"
 #define _SEMVER_CORE "0.9.1"
-#define _SEMVER_BUILD "0.9.1+1317"
-#define _SEMVER_GITHASH "0.9.1+7f41cd0"
-#define _SEMVER_FULL "0.9.1-beta+7f41cd0"
-#define _SEMVER_NOBUILD "0.9.1-beta (12-12-2021)"
-#define _VERSION "0.9.1-beta+7f41cd0 (12-12-2021)"
+#define _SEMVER_BUILD "0.9.1+1320"
+#define _SEMVER_GITHASH "0.9.1+3ca8c24"
+#define _SEMVER_FULL "0.9.1-beta+3ca8c24"
+#define _SEMVER_NOBUILD "0.9.1-beta (15-12-2021)"
+#define _VERSION "0.9.1-beta+3ca8c24 (15-12-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
