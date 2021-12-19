ac678c95f49dceb88c265738d5a062e128ecda03
Robert van den Breemen <robert@vandenbreemen.net>
2021-12-19 20:47:20 +0100

    Added fractional micros to debug time

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 1391
-#define _VERSION_GITHASH "06947d2"
+#define _VERSION_BUILD 1418
+#define _VERSION_GITHASH "4219671"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "19-12-2021"
-#define _VERSION_TIME "17:29:25"
+#define _VERSION_TIME "20:36:00"
 #define _SEMVER_CORE "0.9.1"
-#define _SEMVER_BUILD "0.9.1+1391"
-#define _SEMVER_GITHASH "0.9.1+06947d2"
-#define _SEMVER_FULL "0.9.1-beta+06947d2"
+#define _SEMVER_BUILD "0.9.1+1418"
+#define _SEMVER_GITHASH "0.9.1+4219671"
+#define _SEMVER_FULL "0.9.1-beta+4219671"
 #define _SEMVER_NOBUILD "0.9.1-beta (19-12-2021)"
-#define _VERSION "0.9.1-beta+06947d2 (19-12-2021)"
+#define _VERSION "0.9.1-beta+4219671 (19-12-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
