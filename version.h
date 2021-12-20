17585124dc5b0187545c7b76bfa142543a1b35f6
Robert van den Breemen <robert@vandenbreemen.net>
2021-12-20 09:24:48 +0100

    update the ot msg spec units

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 1424
-#define _VERSION_GITHASH "504cfce"
+#define _VERSION_BUILD 1425
+#define _VERSION_GITHASH "00981da"
 #define _VERSION_PRERELEASE rc1
 #define _VERSION_DATE "19-12-2021"
-#define _VERSION_TIME "21:28:58"
+#define _VERSION_TIME "21:30:34"
 #define _SEMVER_CORE "0.9.1"
-#define _SEMVER_BUILD "0.9.1+1424"
-#define _SEMVER_GITHASH "0.9.1+504cfce"
-#define _SEMVER_FULL "0.9.1-rc1+504cfce"
+#define _SEMVER_BUILD "0.9.1+1425"
+#define _SEMVER_GITHASH "0.9.1+00981da"
+#define _SEMVER_FULL "0.9.1-rc1+00981da"
 #define _SEMVER_NOBUILD "0.9.1-rc1 (19-12-2021)"
-#define _VERSION "0.9.1-rc1+504cfce (19-12-2021)"
+#define _VERSION "0.9.1-rc1+00981da (19-12-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
