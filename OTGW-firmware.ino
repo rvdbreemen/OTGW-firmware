c8ba41453e9104fe3d5a80e106df50bef4e27a90
Robert van den Breemen <robert@vandenbreemen.net>
2021-12-27 19:50:21 +0100

    Change to main loop and timers (no more catchup, better to skip)

--- OTGW-firmware.ino
+++ OTGW-firmware.ino
@@ -275,20 +275,20 @@ void doBackgroundTasks()
 
 void loop()
 {
-  DECLARE_TIMER_SEC(timer1s, 1, CATCH_UP_MISSED_TICKS);
-  DECLARE_TIMER_SEC(timer5s, 5, CATCH_UP_MISSED_TICKS);
+  DECLARE_TIMER_SEC(timer1s, 1, SKIP_MISSED_TICKS);
+  DECLARE_TIMER_SEC(timer5s, 5, SKIP_MISSED_TICKS);
   DECLARE_TIMER_SEC(timer30s, 30, CATCH_UP_MISSED_TICKS);
   DECLARE_TIMER_SEC(timer60s, 60, CATCH_UP_MISSED_TICKS);
   DECLARE_TIMER_MIN(tmrcheckpic, 1440, CATCH_UP_MISSED_TICKS);
   DECLARE_TIMER_MIN(timer5min, 5, CATCH_UP_MISSED_TICKS);
   
-  if (DUE(timer1s))         doTaskEvery1s();
-  if (DUE(timer5s))         doTaskEvery5s();
-  if (DUE(timer30s))        doTaskEvery30s();
-  if (DUE(timer60s))        doTaskEvery60s();
-  if (DUE(tmrcheckpic))     docheckforpic();
-  if (DUE(timer5min))       do5minevent();
   if (DUE(timerpollsensor)) pollSensors();    // poll the temperature sensors connected to 2wire gpio pin 
+  if (DUE(timer5min))       do5minevent();
+  if (DUE(timer60s))        doTaskEvery60s();
+  if (DUE(timer30s))        doTaskEvery30s();
+  if (DUE(timer5s))         doTaskEvery5s();
+  if (DUE(timer1s))         doTaskEvery1s();
+  if (DUE(tmrcheckpic))     docheckforpic();
   evalOutputs();                              // when the bits change, the output gpio bit will follow
   doBackgroundTasks();
 }
