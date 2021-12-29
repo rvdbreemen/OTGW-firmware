75eb55d1a10487d97a00585d6cb6b5dce34c3fb2
Robert van den Breemen <robert@vandenbreemen.net>
2021-12-29 18:59:04 +0100

    removing yield

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 1472
-#define _VERSION_GITHASH "bee9bd1"
+#define _VERSION_BUILD 1507
+#define _VERSION_GITHASH "c8ba414"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "27-12-2021"
-#define _VERSION_TIME "01:16:45"
+#define _VERSION_DATE "29-12-2021"
+#define _VERSION_TIME "18:10:41"
 #define _SEMVER_CORE "0.9.2"
-#define _SEMVER_BUILD "0.9.2+1472"
-#define _SEMVER_GITHASH "0.9.2+bee9bd1"
-#define _SEMVER_FULL "0.9.2-beta+bee9bd1"
-#define _SEMVER_NOBUILD "0.9.2-beta (27-12-2021)"
-#define _VERSION "0.9.2-beta+bee9bd1 (27-12-2021)"
+#define _SEMVER_BUILD "0.9.2+1507"
+#define _SEMVER_GITHASH "0.9.2+c8ba414"
+#define _SEMVER_FULL "0.9.2-beta+c8ba414"
+#define _SEMVER_NOBUILD "0.9.2-beta (29-12-2021)"
+#define _VERSION "0.9.2-beta+c8ba414 (29-12-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
