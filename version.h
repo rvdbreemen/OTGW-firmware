05ec1756985476303c6d145a6294a925707c259e
Robert van den Breemen <robert@vandenbreemen.net>
2021-12-30 14:14:26 +0100

    settling for gmt debug time for now

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 1517
-#define _VERSION_GITHASH "3e729fc"
+#define _VERSION_BUILD 1527
+#define _VERSION_GITHASH "352587c"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "30-12-2021"
-#define _VERSION_TIME "00:38:02"
+#define _VERSION_TIME "13:55:21"
 #define _SEMVER_CORE "0.9.2"
-#define _SEMVER_BUILD "0.9.2+1517"
-#define _SEMVER_GITHASH "0.9.2+3e729fc"
-#define _SEMVER_FULL "0.9.2-beta+3e729fc"
+#define _SEMVER_BUILD "0.9.2+1527"
+#define _SEMVER_GITHASH "0.9.2+352587c"
+#define _SEMVER_FULL "0.9.2-beta+352587c"
 #define _SEMVER_NOBUILD "0.9.2-beta (30-12-2021)"
-#define _VERSION "0.9.2-beta+3e729fc (30-12-2021)"
+#define _VERSION "0.9.2-beta+352587c (30-12-2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
