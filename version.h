a7b4eb1a3e89978844394aa09e2b7d0701f351c3
Robert van den Breemen <robert@vandenbreemen.net>
2022-01-04 01:24:04 +0100

    fixing doAutoconfigure for all ids

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 2
-#define _VERSION_BUILD 1557
-#define _VERSION_GITHASH "0419734"
+#define _VERSION_BUILD 1565
+#define _VERSION_GITHASH "6bc1df7"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "04-01-2022"
-#define _VERSION_TIME "00:49:41"
+#define _VERSION_TIME "01:23:28"
 #define _SEMVER_CORE "0.9.2"
-#define _SEMVER_BUILD "0.9.2+1557"
-#define _SEMVER_GITHASH "0.9.2+0419734"
-#define _SEMVER_FULL "0.9.2-beta+0419734"
+#define _SEMVER_BUILD "0.9.2+1565"
+#define _SEMVER_GITHASH "0.9.2+6bc1df7"
+#define _SEMVER_FULL "0.9.2-beta+6bc1df7"
 #define _SEMVER_NOBUILD "0.9.2-beta (04-01-2022)"
-#define _VERSION "0.9.2-beta+0419734 (04-01-2022)"
+#define _VERSION "0.9.2-beta+6bc1df7 (04-01-2022)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
