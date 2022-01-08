e9c35e9032ed7b5dbcb484f745d1c813d9af8fb6
Robert van den Breemen <robert@vandenbreemen.net>
2022-01-08 12:52:37 +0100

    update version to 0.9.2

--- OTGW-firmware.h
+++ OTGW-firmware.h
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program  : OTGW-firmware.h
-**  Version  : v0.9.2-beta
+**  Version  : v0.9.2
 **
 **  Copyright (c) 2021-2022 Robert van den Breemen
 **
--- outputs_ext.ino
+++ outputs_ext.ino
@@ -1,6 +1,6 @@
 /*********
 **  Program  : output_ext.ino
-**  Version  : v0.9.2-beta
+**  Version  : v0.9.2
 **
 **  Copyright (c) 2021-2022 Robert van den Breemen
 **  Contributed by Sjorsjuhmaniac
