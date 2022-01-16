4b5c2b2b5ec488a10503fe5d040afeb329c11a07
Robert van den Breemen <robert@vandenbreemen.net>
2022-01-16 10:18:39 +0100

    Bigger serial input buffer
    Bigger input buffer, as before. Back to 256.

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 1626
-#define _VERSION_GITHASH "0529d7f"
+#define _VERSION_BUILD 1627
+#define _VERSION_GITHASH "2db98fc"
 #define _VERSION_PRERELEASE experimental
-#define _VERSION_DATE "14-01-2022"
-#define _VERSION_TIME "07:58:12"
+#define _VERSION_DATE "15-01-2022"
+#define _VERSION_TIME "14:43:04"
 #define _SEMVER_CORE "0.9.3"
-#define _SEMVER_BUILD "0.9.3+1626"
-#define _SEMVER_GITHASH "0.9.3+0529d7f"
-#define _SEMVER_FULL "0.9.3-experimental+0529d7f"
-#define _SEMVER_NOBUILD "0.9.3-experimental (14-01-2022)"
-#define _VERSION "0.9.3-experimental+0529d7f (14-01-2022)"
+#define _SEMVER_BUILD "0.9.3+1627"
+#define _SEMVER_GITHASH "0.9.3+2db98fc"
+#define _SEMVER_FULL "0.9.3-experimental+2db98fc"
+#define _SEMVER_NOBUILD "0.9.3-experimental (15-01-2022)"
+#define _VERSION "0.9.3-experimental+2db98fc (15-01-2022)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
