b4b8026f5f9a16be615f6a408893cf672b3e96de
Robert van den Breemen <robert@vandenbreemen.net>
2022-01-16 19:48:10 +0100

    update version to 0.9.3

--- settingStuff.ino
+++ settingStuff.ino
@@ -1,7 +1,7 @@
 /*
 ***************************************************************************  
 **  Program  : settingsStuff
-**  Version  : v0.9.3-beta
+**  Version  : v0.9.3
 **
 **  Copyright (c) 2021-2022 Robert van den Breemen
 **     based on Framework ESP8266 from Willem Aandewiel
