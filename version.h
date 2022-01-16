b4c22859751dc067a9c48bd2605e70f2669f07d2
Robert van den Breemen <robert@vandenbreemen.net>
2022-01-16 22:47:38 +0100

    Update after release... just version and hash

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 1635
-#define _VERSION_GITHASH "f7968e7"
+#define _VERSION_BUILD 1638
+#define _VERSION_GITHASH "b4b8026"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "16-01-2022"
-#define _VERSION_TIME "19:46:38"
+#define _VERSION_TIME "21:09:00"
 #define _SEMVER_CORE "0.9.3"
-#define _SEMVER_BUILD "0.9.3+1635"
-#define _SEMVER_GITHASH "0.9.3+f7968e7"
-#define _SEMVER_FULL "0.9.3+f7968e7"
+#define _SEMVER_BUILD "0.9.3+1638"
+#define _SEMVER_GITHASH "0.9.3+b4b8026"
+#define _SEMVER_FULL "0.9.3+b4b8026"
 #define _SEMVER_NOBUILD "0.9.3 (16-01-2022)"
-#define _VERSION "0.9.3+f7968e7 (16-01-2022)"
+#define _VERSION "0.9.3+b4b8026 (16-01-2022)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
