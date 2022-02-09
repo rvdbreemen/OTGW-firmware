b7a131f060bf7522e0e413ca5856ce20c8e67cc5
Robbert Lagreweij <rlagerweij@hotmail.com>
2022-02-09 16:43:07 +0100

    include build scripts for the littlefs filesystem

---
+++ build_littlefs.bat
@@ -0,0 +1,14 @@
+@echo off
+set "mklittlefs_exec=%homedrive%%homepath%\AppData\Local\Arduino15\packages\esp8266\tools\mklittlefs\2.5.0-4-fe5bb56\mklittlefs.exe"
+set /A block_size = 4096
+set /A page_size = 512
+set /A fs_size = 1024 * 1024
+set "source_dir=data"
+set "img_name=OTGW-Firmware.littlefs.1M.bin"
+set "build_command=%mklittlefs_exec% -c %source_dir% -b %block_size% -p %page_size% -s %fs_size% %img_name%"
+call %build_command%
+pause
+set "check_command=%mklittlefs_exec% -l  -b %block_size% -p %page_size% -s %fs_size% %img_name%"
+call %check_command%
+
+
