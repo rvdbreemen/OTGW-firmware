46782991e5b6edae0c24eddd4f370abf805314d6
Robert van den Breemen <robert@vandenbreemen.net>
2022-02-16 20:13:12 +0100

    adding 1.5MB version of SystemFS

--- version.h
+++ version.h
@@ -1,16 +1,16 @@
 //The version number conforms to semver.org format
-#define _VERSION_MAJOR 0
-#define _VERSION_MINOR 9  
-#define _VERSION_PATCH 3
-#define _VERSION_BUILD 1638
-#define _VERSION_GITHASH "b4b8026"
+#define _VERSION_MAJOR 1
+#define _VERSION_MINOR 0  
+#define _VERSION_PATCH 0
+#define _VERSION_BUILD 1642
+#define _VERSION_GITHASH "0003867"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "16-01-2022"
-#define _VERSION_TIME "21:09:00"
-#define _SEMVER_CORE "0.9.3"
-#define _SEMVER_BUILD "0.9.3+1638"
-#define _SEMVER_GITHASH "0.9.3+b4b8026"
-#define _SEMVER_FULL "0.9.3+b4b8026"
-#define _SEMVER_NOBUILD "0.9.3 (16-01-2022)"
-#define _VERSION "0.9.3+b4b8026 (16-01-2022)"
+#define _VERSION_DATE "15-02-2022"
+#define _VERSION_TIME "21:01:54"
+#define _SEMVER_CORE "1.0.0"
+#define _SEMVER_BUILD "1.0.0+1642"
+#define _SEMVER_GITHASH "1.0.0+0003867"
+#define _SEMVER_FULL "1.0.0+0003867"
+#define _SEMVER_NOBUILD "1.0.0 (15-02-2022)"
+#define _VERSION "1.0.0+0003867 (15-02-2022)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
