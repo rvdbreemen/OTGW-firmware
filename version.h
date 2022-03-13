ac02f1b7e8af5d437cfaf3c52fb2e07de3a20c8f
Robert van den Breemen <robert@vandenbreemen.net>
2022-03-13 23:52:16 +0100

    Making sure the upgrade never happens, when it detects a firmware 6.x
    This current release does not support upgrading P16F147, so anything that has 6.x firmware on it is not supported by the upgrade routine.

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 4
-#define _VERSION_BUILD 1652
-#define _VERSION_GITHASH "07e0e31"
+#define _VERSION_BUILD 1655
+#define _VERSION_GITHASH "c3e012c"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "13-03-2022"
-#define _VERSION_TIME "23:07:38"
+#define _VERSION_TIME "23:47:51"
 #define _SEMVER_CORE "0.9.4"
-#define _SEMVER_BUILD "0.9.4+1652"
-#define _SEMVER_GITHASH "0.9.4+07e0e31"
-#define _SEMVER_FULL "0.9.4+07e0e31"
+#define _SEMVER_BUILD "0.9.4+1655"
+#define _SEMVER_GITHASH "0.9.4+c3e012c"
+#define _SEMVER_FULL "0.9.4+c3e012c"
 #define _SEMVER_NOBUILD "0.9.4 (13-03-2022)"
-#define _VERSION "0.9.4+07e0e31 (13-03-2022)"
+#define _VERSION "0.9.4+c3e012c (13-03-2022)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
