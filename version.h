d3df38ea5dcdb68e42283dc63f938854396bda4d
Robert van den Breemen <robert@vandenbreemen.net>
2022-03-13 22:25:13 +0100

    update gateway to 5.3 in FS and prevent 6.x upgrades

--- version.h
+++ version.h
@@ -1,16 +1,16 @@
 //The version number conforms to semver.org format
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
-#define _VERSION_PATCH 3
-#define _VERSION_BUILD 1638
-#define _VERSION_GITHASH "b4b8026"
+#define _VERSION_PATCH 4
+#define _VERSION_BUILD 1641
+#define _VERSION_GITHASH "158a6ce"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "16-01-2022"
-#define _VERSION_TIME "21:09:00"
-#define _SEMVER_CORE "0.9.3"
-#define _SEMVER_BUILD "0.9.3+1638"
-#define _SEMVER_GITHASH "0.9.3+b4b8026"
-#define _SEMVER_FULL "0.9.3+b4b8026"
-#define _SEMVER_NOBUILD "0.9.3 (16-01-2022)"
-#define _VERSION "0.9.3+b4b8026 (16-01-2022)"
+#define _VERSION_DATE "13-03-2022"
+#define _VERSION_TIME "22:19:22"
+#define _SEMVER_CORE "0.9.4"
+#define _SEMVER_BUILD "0.9.4+1641"
+#define _SEMVER_GITHASH "0.9.4+158a6ce"
+#define _SEMVER_FULL "0.9.4+158a6ce"
+#define _SEMVER_NOBUILD "0.9.4 (13-03-2022)"
+#define _VERSION "0.9.4+158a6ce (13-03-2022)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
