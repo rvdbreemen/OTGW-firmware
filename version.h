281d265783a4e918265d14620d56ff42ac3d3339
Robert van den Breemen <robert@vandenbreemen.net>
2022-03-13 23:01:35 +0100

    prevented update to 6.x firmware for now

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 4
-#define _VERSION_BUILD 1641
-#define _VERSION_GITHASH "158a6ce"
+#define _VERSION_BUILD 1649
+#define _VERSION_GITHASH "d0aa426"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "13-03-2022"
-#define _VERSION_TIME "22:19:22"
+#define _VERSION_TIME "22:55:48"
 #define _SEMVER_CORE "0.9.4"
-#define _SEMVER_BUILD "0.9.4+1641"
-#define _SEMVER_GITHASH "0.9.4+158a6ce"
-#define _SEMVER_FULL "0.9.4+158a6ce"
+#define _SEMVER_BUILD "0.9.4+1649"
+#define _SEMVER_GITHASH "0.9.4+d0aa426"
+#define _SEMVER_FULL "0.9.4+d0aa426"
 #define _SEMVER_NOBUILD "0.9.4 (13-03-2022)"
-#define _VERSION "0.9.4+158a6ce (13-03-2022)"
+#define _VERSION "0.9.4+d0aa426 (13-03-2022)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
