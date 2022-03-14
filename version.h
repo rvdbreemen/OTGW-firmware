0011cde855bbb72ce75d8269de4dde548a27306d
Robert van den Breemen <robert@vandenbreemen.net>
2022-03-14 19:32:46 +0100

    Fix for if issue

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 4
-#define _VERSION_BUILD 1655
-#define _VERSION_GITHASH "c3e012c"
+#define _VERSION_BUILD 1658
+#define _VERSION_GITHASH "f009cfe"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "13-03-2022"
-#define _VERSION_TIME "23:47:51"
+#define _VERSION_DATE "14-03-2022"
+#define _VERSION_TIME "19:13:07"
 #define _SEMVER_CORE "0.9.4"
-#define _SEMVER_BUILD "0.9.4+1655"
-#define _SEMVER_GITHASH "0.9.4+c3e012c"
-#define _SEMVER_FULL "0.9.4+c3e012c"
-#define _SEMVER_NOBUILD "0.9.4 (13-03-2022)"
-#define _VERSION "0.9.4+c3e012c (13-03-2022)"
+#define _SEMVER_BUILD "0.9.4+1658"
+#define _SEMVER_GITHASH "0.9.4+f009cfe"
+#define _SEMVER_FULL "0.9.4+f009cfe"
+#define _SEMVER_NOBUILD "0.9.4 (14-03-2022)"
+#define _VERSION "0.9.4+f009cfe (14-03-2022)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
