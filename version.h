3a11865043868b9fe32f7a55d2873172f04c0ac6
Robert van den Breemen <robert@vandenbreemen.net>
2022-05-28 10:31:04 +0200

    on reboot, check after 5 minutes

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 5
-#define _VERSION_BUILD 1684
-#define _VERSION_GITHASH "091868b"
+#define _VERSION_BUILD 1685
+#define _VERSION_GITHASH "c145f64"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "28-05-2022"
-#define _VERSION_TIME "10:29:38"
+#define _VERSION_TIME "10:30:51"
 #define _SEMVER_CORE "0.9.5"
-#define _SEMVER_BUILD "0.9.5+1684"
-#define _SEMVER_GITHASH "0.9.5+091868b"
-#define _SEMVER_FULL "0.9.5+091868b"
+#define _SEMVER_BUILD "0.9.5+1685"
+#define _SEMVER_GITHASH "0.9.5+c145f64"
+#define _SEMVER_FULL "0.9.5+c145f64"
 #define _SEMVER_NOBUILD "0.9.5 (28-05-2022)"
-#define _VERSION "0.9.5+091868b (28-05-2022)"
+#define _VERSION "0.9.5+c145f64 (28-05-2022)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
