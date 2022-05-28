f56140864d3dd3d1110bc96fb2d6df774cc22336
Robert van den Breemen <robert@vandenbreemen.net>
2022-05-28 10:31:04 +0200

    on reboot, check after 5 minutes

--- OTGW-firmware.h
+++ OTGW-firmware.h
@@ -94,7 +94,7 @@ bool      bOTGWboilerstate = false;
 bool      bOTGWthermostatstate = false;
 bool      bOTGWgatewaystate = false;
 bool      bPrintSummarymode = false;  //default to PS=0 mode
-bool      bCheckOTGWPICupdate = false;
+bool      bCheckOTGWPICupdate = true;  
 
 //All things that are settings 
 String    settingHostname = _HOSTNAME;
