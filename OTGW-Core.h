cb752dd43e0ee50090fb0e3b4522a14ad7bbc9c0
Robert van den Breemen <robert@vandenbreemen.net>
2022-05-30 08:27:46 +0200

    update version to 0.9.5

--- OTGW-Core.h
+++ OTGW-Core.h
@@ -1,7 +1,7 @@
 /*
 ***************************************************************************  
 **  Program  : Header file: OTGW-Core.h 
-**  Version  : v0.9.4
+**  Version  : v0.9.5
 **
 **  Copyright (c) 2021-2022 Robert van den Breemen
 **  Borrowed from OpenTherm library from: 
