4f995f54b6973b961f6f9df61df945e29e563e47
Robert van den Breemen <robert@vandenbreemen.net>
2022-06-04 12:14:10 +0200

    Remove strcasecmp for GW=R
    reported by Schelte

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 5
-#define _VERSION_BUILD 1727
-#define _VERSION_GITHASH "88d2c81"
+#define _VERSION_BUILD 1728
+#define _VERSION_GITHASH "6fb095d"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "04-06-2022"
-#define _VERSION_TIME "09:49:33"
+#define _VERSION_TIME "12:13:34"
 #define _SEMVER_CORE "0.9.5"
-#define _SEMVER_BUILD "0.9.5+1727"
-#define _SEMVER_GITHASH "0.9.5+88d2c81"
-#define _SEMVER_FULL "0.9.5+88d2c81"
+#define _SEMVER_BUILD "0.9.5+1728"
+#define _SEMVER_GITHASH "0.9.5+6fb095d"
+#define _SEMVER_FULL "0.9.5+6fb095d"
 #define _SEMVER_NOBUILD "0.9.5 (04-06-2022)"
-#define _VERSION "0.9.5+88d2c81 (04-06-2022)"
+#define _VERSION "0.9.5+6fb095d (04-06-2022)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
