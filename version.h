aea78a32e0ef7f744763289da1a4305cf01c3ccf
Robert van den Breemen <robert@vandenbreemen.net>
2022-06-07 22:32:17 +0200

    remove unknown device id block

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 5
-#define _VERSION_BUILD 1737
-#define _VERSION_GITHASH "5362b59"
+#define _VERSION_BUILD 1738
+#define _VERSION_GITHASH "138387c"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "07-06-2022"
-#define _VERSION_TIME "19:33:03"
+#define _VERSION_TIME "19:39:11"
 #define _SEMVER_CORE "0.9.5"
-#define _SEMVER_BUILD "0.9.5+1737"
-#define _SEMVER_GITHASH "0.9.5+5362b59"
-#define _SEMVER_FULL "0.9.5+5362b59"
+#define _SEMVER_BUILD "0.9.5+1738"
+#define _SEMVER_GITHASH "0.9.5+138387c"
+#define _SEMVER_FULL "0.9.5+138387c"
 #define _SEMVER_NOBUILD "0.9.5 (07-06-2022)"
-#define _VERSION "0.9.5+5362b59 (07-06-2022)"
+#define _VERSION "0.9.5+138387c (07-06-2022)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
