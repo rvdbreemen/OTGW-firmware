01821dcb09842cb06fea12633d760f72fa97cb4a
Robert van den Breemen <robert@vandenbreemen.net>
2022-06-15 07:21:48 +0200

    update version

--- version.h
+++ version.h
@@ -1,16 +1,16 @@
 //The version number conforms to semver.org format
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
-#define _VERSION_PATCH 5
-#define _VERSION_BUILD 1687
-#define _VERSION_GITHASH "960aed0"
-//#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
+#define _VERSION_PATCH 6
+#define _VERSION_BUILD 1688
+#define _VERSION_GITHASH "c9479f3"
+#define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "15-06-2022"
-#define _VERSION_TIME "07:19:32"
-#define _SEMVER_CORE "0.9.5"
-#define _SEMVER_BUILD "0.9.5+1687"
-#define _SEMVER_GITHASH "0.9.5+960aed0"
-#define _SEMVER_FULL "0.9.5+960aed0"
-#define _SEMVER_NOBUILD "0.9.5 (15-06-2022)"
-#define _VERSION "0.9.5+960aed0 (15-06-2022)"
+#define _VERSION_TIME "07:21:38"
+#define _SEMVER_CORE "0.9.6"
+#define _SEMVER_BUILD "0.9.6+1688"
+#define _SEMVER_GITHASH "0.9.6+c9479f3"
+#define _SEMVER_FULL "0.9.6-beta+c9479f3"
+#define _SEMVER_NOBUILD "0.9.6-beta (15-06-2022)"
+#define _VERSION "0.9.6-beta+c9479f3 (15-06-2022)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
