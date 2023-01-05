3ff694347e6dfce4ae09d85668c842ac3066550e
Rob Roos <mail@roos-aid.nl>
2023-01-05 21:27:46 +0100

    Merge changes RB

--- version.h
+++ version.h
@@ -2,14 +2,20 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 1687
+#define _VERSION_BUILD 1777
 #define _VERSION_GITHASH "4eb7d24"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "27/08/2022"
-#define _VERSION_TIME "13:09:23"
+#define _VERSION_DATE "24/12/2022"
+#define _VERSION_TIME "21:22:31"
 #define _SEMVER_CORE "0.10.1"
 #define _SEMVER_NOBUILD "0.10.1"
 #define _SEMVER_CORE "0.10.1"
+<<<<<<< Updated upstream
 #define _SEMVER_FULL "0.10.1 (27/08/2022)"
 #define _VERSION "0.10.1+1687 (27/08/2022)"
+=======
+#define _SEMVER_FULL "0.10.1 (24/12/2022"
+#define _VERSION "0.10.1+1777 (24/12/2022)"
+>>>>>>> dev_pulsectr
+>>>>>>> Stashed changes
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
