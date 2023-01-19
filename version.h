842ff79046658e0181fdbcaff5925b8157228184
Robert van den Breemen <robert@vandenbreemen.net>
2023-01-19 22:40:50 +0100

    Adding restapi for fw type and fixing UI

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 6
-#define _VERSION_BUILD 1887
-#define _VERSION_GITHASH "5495bbc"
+#define _VERSION_BUILD 1888
+#define _VERSION_GITHASH "2d0dc79"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "19-01-2023"
-#define _VERSION_TIME "22:36:14"
+#define _VERSION_TIME "22:39:29"
 #define _SEMVER_CORE "0.9.6"
-#define _SEMVER_BUILD "0.9.6+1887"
-#define _SEMVER_GITHASH "0.9.6+5495bbc"
-#define _SEMVER_FULL "0.9.6-beta+5495bbc"
+#define _SEMVER_BUILD "0.9.6+1888"
+#define _SEMVER_GITHASH "0.9.6+2d0dc79"
+#define _SEMVER_FULL "0.9.6-beta+2d0dc79"
 #define _SEMVER_NOBUILD "0.9.6-beta (19-01-2023)"
-#define _VERSION "0.9.6-beta+5495bbc (19-01-2023)"
+#define _VERSION "0.9.6-beta+2d0dc79 (19-01-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
