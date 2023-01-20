059c664472f784787a094e6aaead320d5614cf58
Robert van den Breemen <robert@vandenbreemen.net>
2023-01-20 23:56:16 +0100

    relocate library to sub in main sketch folder

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 9  
 #define _VERSION_PATCH 6
-#define _VERSION_BUILD 1895
-#define _VERSION_GITHASH "46c99c2"
+#define _VERSION_BUILD 1896
+#define _VERSION_GITHASH "3667cba"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "19-01-2023"
-#define _VERSION_TIME "23:23:23"
+#define _VERSION_DATE "20-01-2023"
+#define _VERSION_TIME "23:48:31"
 #define _SEMVER_CORE "0.9.6"
-#define _SEMVER_BUILD "0.9.6+1895"
-#define _SEMVER_GITHASH "0.9.6+46c99c2"
-#define _SEMVER_FULL "0.9.6-beta+46c99c2"
-#define _SEMVER_NOBUILD "0.9.6-beta (19-01-2023)"
-#define _VERSION "0.9.6-beta+46c99c2 (19-01-2023)"
+#define _SEMVER_BUILD "0.9.6+1896"
+#define _SEMVER_GITHASH "0.9.6+3667cba"
+#define _SEMVER_FULL "0.9.6-beta+3667cba"
+#define _SEMVER_NOBUILD "0.9.6-beta (20-01-2023)"
+#define _VERSION "0.9.6-beta+3667cba (20-01-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
