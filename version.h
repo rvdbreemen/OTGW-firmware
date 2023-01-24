fc165b13c9758f7829ba54bf1bd009265a672b92
Rob Roos <mail@roos-aid.nl>
2023-01-24 21:06:07 +0100

    Moved debugstring to flash

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 1957
+#define _VERSION_BUILD 1959
 #define _VERSION_GITHASH "0000000"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "24/01/2023"
-#define _VERSION_TIME "20:50:53"
+#define _VERSION_TIME "21:05:09"
 #define _SEMVER_CORE "0.10.1"
-#define _SEMVER_BUILD "0.10.1+1957"
+#define _SEMVER_BUILD "0.10.1+1959"
 #define _SEMVER_GITHASH "0.10.1+0000000"
 #define _SEMVER_FULL "0.10.1 beta (24/01/2023)"
 #define _SEMVER_NOBUILD "0.10.1 (24/01/2023)"
-#define _VERSION "0.10.1+1957 (24/01/2023)"
+#define _VERSION "0.10.1+1959 (24/01/2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
