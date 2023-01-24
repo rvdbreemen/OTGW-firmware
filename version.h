56d058858e74f0ca1c98d9e74685db6862768ff4
Robert van den Breemen <robert@vandenbreemen.net>
2023-01-24 22:00:33 +0100

    Fixing readme before release, plus updated to version 0.10.0

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 //The version number conforms to semver.org format
 #define _VERSION_MAJOR 0
-#define _VERSION_MINOR 9  
-#define _VERSION_PATCH 6
+#define _VERSION_MINOR 10  
+#define _VERSION_PATCH 0
 #define _VERSION_BUILD 1955
 #define _VERSION_GITHASH "2fe25ed"
 #define _VERSION_PRERELEASE beta
