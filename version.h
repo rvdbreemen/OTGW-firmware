e3b898c545520f39f48cf0941e089965a795f188
Robert van den Breemen <robert@vandenbreemen.net>
2023-01-24 22:11:36 +0100

    Bugfix not PTSTR, but F flash macro Tx @hvxl

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 1956
-#define _VERSION_GITHASH "56d0588"
+#define _VERSION_BUILD 1957
+#define _VERSION_GITHASH "035e834"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "24-01-2023"
-#define _VERSION_TIME "22:03:18"
+#define _VERSION_TIME "22:05:02"
 #define _SEMVER_CORE "0.10.0"
-#define _SEMVER_BUILD "0.10.0+1956"
-#define _SEMVER_GITHASH "0.10.0+56d0588"
-#define _SEMVER_FULL "0.10.0-beta+56d0588"
+#define _SEMVER_BUILD "0.10.0+1957"
+#define _SEMVER_GITHASH "0.10.0+035e834"
+#define _SEMVER_FULL "0.10.0-beta+035e834"
 #define _SEMVER_NOBUILD "0.10.0-beta (24-01-2023)"
-#define _VERSION "0.10.0-beta+56d0588 (24-01-2023)"
+#define _VERSION "0.10.0-beta+035e834 (24-01-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
