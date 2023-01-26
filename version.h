9e01bef4bc3d977b38793e299e6f7c2a9ea6085d
Robert van den Breemen <robert@vandenbreemen.net>
2023-01-26 20:15:47 +0100

    Adding some readme release notes

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 1975
-#define _VERSION_GITHASH "11a00e3"
+#define _VERSION_BUILD 1976
+#define _VERSION_GITHASH "dc8ee54"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "26-01-2023"
-#define _VERSION_TIME "15:24:48"
+#define _VERSION_TIME "20:12:46"
 #define _SEMVER_CORE "0.10.0"
-#define _SEMVER_BUILD "0.10.0+1975"
-#define _SEMVER_GITHASH "0.10.0+11a00e3"
-#define _SEMVER_FULL "0.10.0-beta+11a00e3"
+#define _SEMVER_BUILD "0.10.0+1976"
+#define _SEMVER_GITHASH "0.10.0+dc8ee54"
+#define _SEMVER_FULL "0.10.0-beta+dc8ee54"
 #define _SEMVER_NOBUILD "0.10.0-beta (26-01-2023)"
-#define _VERSION "0.10.0-beta+11a00e3 (26-01-2023)"
+#define _VERSION "0.10.0-beta+dc8ee54 (26-01-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
