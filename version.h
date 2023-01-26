60ff8011dcfa5d3bbfa5aaf3f21327237517da86
Robert van den Breemen <robert@vandenbreemen.net>
2023-01-26 04:26:07 +0100

    change formatfloat to string class

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 1964
-#define _VERSION_GITHASH "e3b898c"
+#define _VERSION_BUILD 1966
+#define _VERSION_GITHASH "00f7a79"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "25-01-2023"
-#define _VERSION_TIME "06:54:43"
+#define _VERSION_DATE "26-01-2023"
+#define _VERSION_TIME "04:24:28"
 #define _SEMVER_CORE "0.10.0"
-#define _SEMVER_BUILD "0.10.0+1964"
-#define _SEMVER_GITHASH "0.10.0+e3b898c"
-#define _SEMVER_FULL "0.10.0-beta+e3b898c"
-#define _SEMVER_NOBUILD "0.10.0-beta (25-01-2023)"
-#define _VERSION "0.10.0-beta+e3b898c (25-01-2023)"
+#define _SEMVER_BUILD "0.10.0+1966"
+#define _SEMVER_GITHASH "0.10.0+00f7a79"
+#define _SEMVER_FULL "0.10.0-beta+00f7a79"
+#define _SEMVER_NOBUILD "0.10.0-beta (26-01-2023)"
+#define _VERSION "0.10.0-beta+00f7a79 (26-01-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
