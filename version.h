08466377e3ac7f317c4d2c1b0cb34cf0e5bee61d
Robert van den Breemen <robert@vandenbreemen.net>
2023-01-26 06:52:44 +0100

    Skip because of parity now added to logging

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 1972
-#define _VERSION_GITHASH "ed0862d"
+#define _VERSION_BUILD 1974
+#define _VERSION_GITHASH "0d0cc15"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "26-01-2023"
-#define _VERSION_TIME "06:47:17"
+#define _VERSION_TIME "06:52:13"
 #define _SEMVER_CORE "0.10.0"
-#define _SEMVER_BUILD "0.10.0+1972"
-#define _SEMVER_GITHASH "0.10.0+ed0862d"
-#define _SEMVER_FULL "0.10.0-beta+ed0862d"
+#define _SEMVER_BUILD "0.10.0+1974"
+#define _SEMVER_GITHASH "0.10.0+0d0cc15"
+#define _SEMVER_FULL "0.10.0-beta+0d0cc15"
 #define _SEMVER_NOBUILD "0.10.0-beta (26-01-2023)"
-#define _VERSION "0.10.0-beta+ed0862d (26-01-2023)"
+#define _VERSION "0.10.0-beta+0d0cc15 (26-01-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
