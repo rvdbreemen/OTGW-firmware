4971aaa355255ccd2e7e7c22efb08b141c819f10
Robert van den Breemen <robert@vandenbreemen.net>
2023-01-28 19:06:48 +0100

    Fixing OEM in MQTT and Home Assistant sensor configuration

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2003
-#define _VERSION_GITHASH "ac599fb"
-#define _VERSION_PRERELEASE rc4
+#define _VERSION_BUILD 2005
+#define _VERSION_GITHASH "3d68912"
+#define _VERSION_PRERELEASE rc5
 #define _VERSION_DATE "28-01-2023"
-#define _VERSION_TIME "11:53:34"
+#define _VERSION_TIME "18:02:54"
 #define _SEMVER_CORE "0.10.0"
-#define _SEMVER_BUILD "0.10.0+2003"
-#define _SEMVER_GITHASH "0.10.0+ac599fb"
-#define _SEMVER_FULL "0.10.0-rc4+ac599fb"
-#define _SEMVER_NOBUILD "0.10.0-rc4 (28-01-2023)"
-#define _VERSION "0.10.0-rc4+ac599fb (28-01-2023)"
+#define _SEMVER_BUILD "0.10.0+2005"
+#define _SEMVER_GITHASH "0.10.0+3d68912"
+#define _SEMVER_FULL "0.10.0-rc5+3d68912"
+#define _SEMVER_NOBUILD "0.10.0-rc5 (28-01-2023)"
+#define _VERSION "0.10.0-rc5+3d68912 (28-01-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
