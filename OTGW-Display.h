2eed8ee6c515779037d9ccdd874b02dd799d1056
Dave Davenport <qball@blame.services>
2023-01-29 19:38:01 +0100

    Add paging support and some demo pages.
    Also start with bar for icons.

--- OTGW-Display.h
+++ OTGW-Display.h
@@ -7,6 +7,12 @@ class OTGW_Display
 {
 
 	public:
+    enum class Pages {
+      DISPLAY_IP,
+      DISPLAY_TIME,
+      DISPLAY_UPTIME,
+      DISPLAY_VERSION
+    };
 		OTGW_Display ();
     ~OTGW_Display ();
 
@@ -23,12 +29,17 @@ class OTGW_Display
   private:
     void draw_welcome();
     void draw_message();
-    void draw_main_screen();
+
+    void draw_display_ip();
+    void draw_display_time();
+    void draw_display_uptime();
+    void draw_display_version();
     
 	private:
     // U8g2 Object used for interaction with Oled.
     U8G2 *u8g2 = nullptr;
     String _message = "";
+    Pages page = Pages::DISPLAY_IP;
 };
 
 #endif // OTGW_DISPLAY
--- OTGW-Display.ino
+++ OTGW-Display.ino
@@ -3,6 +3,7 @@
 #include "Wire.h"
 #include "Debug.h"
 #include "clib/u8g2.h"
+#include "OTGW-Core.h"
 
 OTGW_Display::~OTGW_Display()
 {
@@ -17,7 +18,7 @@ void OTGW_Display::draw_welcome()
     u8g2->drawUTF8(4,7, "OTGW Booting..");
     char buf[16];
     snprintf(buf, sizeof(buf), "Version %s", _VERSION );
-    u8g2->drawUTF8(4,20, buf );
+    u8g2->drawUTF8(4,24, buf );
     u8g2->sendBuffer();
 }
 
@@ -46,7 +47,7 @@ void OTGW_Display::begin()
     u8g2->setContrast(1);
 
     // Set some defaults
-    u8g2->setFont(u8g2_font_cu12_tf);
+    u8g2->setFont(u8g2_font_t0_14b_mr);
     u8g2->setFontRefHeightExtendedText();
     u8g2->setDrawColor(1);
     u8g2->setFontPosTop();
@@ -61,12 +62,59 @@ void OTGW_Display::draw_message()
 
 }
 
-void OTGW_Display::draw_main_screen()
+void OTGW_Display::draw_display_ip()
 {
 	char buf[32];
-	snprintf(buf, sizeof(buf), "IP: %d.%d.%d.%d", WiFi.localIP()[0], WiFi.localIP()[1], WiFi.localIP()[2], WiFi.localIP()[3]);
-  u8g2->drawUTF8(4,7, buf);
+	snprintf(buf, sizeof(buf), "IP");
+  u8g2->drawUTF8(4,4, buf);
+	snprintf(buf, sizeof(buf), "%d.%d.%d.%d", WiFi.localIP()[0], WiFi.localIP()[1], WiFi.localIP()[2], WiFi.localIP()[3]);
+  u8g2->drawUTF8(4,24, buf);
 }
+
+void OTGW_Display::draw_display_time()
+{
+
+  char buf[50];
+	snprintf(buf, sizeof(buf), "Time");
+  u8g2->drawUTF8(4,4, buf);
+  time_t now = time(nullptr);
+  //Timezone based devtime
+  TimeZone myTz =  timezoneManager.createForZoneName(CSTR(settingNTPtimezone));
+  ZonedDateTime myTime = ZonedDateTime::forUnixSeconds64(now, myTz);
+  snprintf(buf, 49, PSTR("%04d-%02d-%02d"), myTime.year(), myTime.month(), myTime.day());
+
+  u8g2->drawUTF8(4,24, buf);
+  snprintf(buf, 49, PSTR("%02d:%02d:%02d"), myTime.hour(), myTime.minute(), myTime.second());
+
+  u8g2->drawUTF8(4,40, buf);
+}
+
+void OTGW_Display::draw_display_uptime()
+{
+
+  char buf[50];
+	snprintf(buf, sizeof(buf), "Uptime");
+  u8g2->drawUTF8(4,4, buf);
+  uint32_t seconds= upTimeSeconds;
+  uint32_t days = seconds/(60*60*24); 
+  seconds -= days*60*60*24;
+  uint16_t hours = seconds/(60*60);
+  seconds -= hours*60*60;
+  uint16_t minutes = seconds/(60);
+  seconds -= minutes*60;
+  snprintf(buf, 49, PSTR("%03dd %02d:%02d:%02d"),
+      days, hours, minutes,seconds);
+
+  u8g2->drawUTF8(4,24, buf);
+}
+void OTGW_Display::draw_display_version()
+{
+  u8g2->drawUTF8(4,4, "OTGW version");
+  char buf[12];
+  snprintf(buf, sizeof(buf), "%s", _VERSION);
+  u8g2->drawUTF8(4,24, buf);
+}
+
 
 void OTGW_Display::tick()
 {
@@ -77,11 +125,35 @@ void OTGW_Display::tick()
   // Draw border around the screen.
   u8g2->clearBuffer();
   u8g2->drawFrame(0,0,128,64);
+  u8g2->drawLine(108, 0,108, 64);
 
   if ( _message.length() > 0 ) {
     draw_message ();
   } else {
-    draw_main_screen ();
+    
+    switch ( page ) {
+      case OTGW_Display::Pages::DISPLAY_IP:
+        draw_display_ip ();
+        page = OTGW_Display::Pages::DISPLAY_TIME;
+        break;
+      case OTGW_Display::Pages::DISPLAY_TIME:
+        draw_display_time ();
+        page = OTGW_Display::Pages::DISPLAY_UPTIME;
+        break;
+      case OTGW_Display::Pages::DISPLAY_UPTIME:
+        draw_display_uptime ();
+        page = OTGW_Display::Pages::DISPLAY_VERSION;
+        break;
+      case OTGW_Display::Pages::DISPLAY_VERSION:
+        draw_display_version ();
+        page = OTGW_Display::Pages::DISPLAY_IP;
+        break;
+    }
+  }
+  if ( ((OTdata.valueLB) & 0x08) ) {
+    u8g2->drawUTF8( 112, 2, "F");
+  } else {
+    u8g2->drawUTF8( 112, 2, "X");
   }
 
   // Send internal buffer towards screen.
