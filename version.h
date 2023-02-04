49199d1459941298af638c6ceda9bfbf27dce38f
Robert van den Breemen <robert@vandenbreemen.net>
2023-02-04 20:27:38 +0100

    Fixing the configuration of OEM fault code in mqttha.cfg

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2016
-#define _VERSION_GITHASH "cab73df"
-#define _VERSION_PRERELEASE dev  //uncomment to define prerelease labels: dev - alpha - beta - rc
-#define _VERSION_DATE "29-01-2023"
-#define _VERSION_TIME "17:34:29"
+#define _VERSION_BUILD 2017
+#define _VERSION_GITHASH "244b604"
+#define _VERSION_PRERELEASE dev
+#define _VERSION_DATE "04-02-2023"
+#define _VERSION_TIME "18:20:14"
 #define _SEMVER_CORE "0.10.0"
-#define _SEMVER_BUILD "0.10.0+2016"
-#define _SEMVER_GITHASH "0.10.0+cab73df"
-#define _SEMVER_FULL "0.10.0+cab73df"
-#define _SEMVER_NOBUILD "0.10.0 (29-01-2023)"
-#define _VERSION "0.10.0+cab73df (29-01-2023)"
+#define _SEMVER_BUILD "0.10.0+2017"
+#define _SEMVER_GITHASH "0.10.0+244b604"
+#define _SEMVER_FULL "0.10.0-dev+244b604"
+#define _SEMVER_NOBUILD "0.10.0-dev (04-02-2023)"
+#define _VERSION "0.10.0-dev+244b604 (04-02-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
