6681e1a5e0d368906e0f80373f2aeb797373f441
Robert van den Breemen <robert@vandenbreemen.net>
2023-02-08 23:57:52 +0100

    rename to littlefs.bin

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 2019
-#define _VERSION_GITHASH "7b22d7d"
+#define _VERSION_BUILD 2020
+#define _VERSION_GITHASH "ed0622a"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "08-02-2023"
-#define _VERSION_TIME "23:36:43"
+#define _VERSION_TIME "23:56:19"
 #define _SEMVER_CORE "0.10.1"
-#define _SEMVER_BUILD "0.10.1+2019"
-#define _SEMVER_GITHASH "0.10.1+7b22d7d"
-#define _SEMVER_FULL "0.10.1-beta+7b22d7d"
+#define _SEMVER_BUILD "0.10.1+2020"
+#define _SEMVER_GITHASH "0.10.1+ed0622a"
+#define _SEMVER_FULL "0.10.1-beta+ed0622a"
 #define _SEMVER_NOBUILD "0.10.1-beta (08-02-2023)"
-#define _VERSION "0.10.1-beta+7b22d7d (08-02-2023)"
+#define _VERSION "0.10.1-beta+ed0622a (08-02-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
