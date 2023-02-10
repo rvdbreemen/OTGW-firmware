cb2171d9748fe2df7d54c8e053e6584093bad384
Robert van den Breemen <robert@vandenbreemen.net>
2023-02-10 21:12:22 +0100

    Add transfer.dat
    Was till missing here... now fixed?

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 2041
-#define _VERSION_GITHASH "2d2d620"
+#define _VERSION_BUILD 2042
+#define _VERSION_GITHASH "6cc0dde"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "10-02-2023"
-#define _VERSION_TIME "07:40:26"
+#define _VERSION_TIME "21:11:23"
 #define _SEMVER_CORE "0.10.1"
-#define _SEMVER_BUILD "0.10.1+2041"
-#define _SEMVER_GITHASH "0.10.1+2d2d620"
-#define _SEMVER_FULL "0.10.1-beta+2d2d620"
+#define _SEMVER_BUILD "0.10.1+2042"
+#define _SEMVER_GITHASH "0.10.1+6cc0dde"
+#define _SEMVER_FULL "0.10.1-beta+6cc0dde"
 #define _SEMVER_NOBUILD "0.10.1-beta (10-02-2023)"
-#define _VERSION "0.10.1-beta+2d2d620 (10-02-2023)"
+#define _VERSION "0.10.1-beta+6cc0dde (10-02-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
