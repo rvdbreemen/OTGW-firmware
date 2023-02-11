aa63902586900ac057e2b1d4d03c4e0838fd8db3
Robert van den Breemen <robert@vandenbreemen.net>
2023-02-11 13:00:10 +0100

    Ignore build process
    When using arduino-cli make file, there will be some folders that can be safely ignored for git push. Like staging and libaries.

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 2046
-#define _VERSION_GITHASH "56994f1"
+#define _VERSION_BUILD 2047
+#define _VERSION_GITHASH "0f35eb6"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
 #define _VERSION_DATE "11-02-2023"
-#define _VERSION_TIME "10:14:06"
+#define _VERSION_TIME "12:57:24"
 #define _SEMVER_CORE "0.10.1"
-#define _SEMVER_BUILD "0.10.1+2046"
-#define _SEMVER_GITHASH "0.10.1+56994f1"
-#define _SEMVER_FULL "0.10.1+56994f1"
+#define _SEMVER_BUILD "0.10.1+2047"
+#define _SEMVER_GITHASH "0.10.1+0f35eb6"
+#define _SEMVER_FULL "0.10.1+0f35eb6"
 #define _SEMVER_NOBUILD "0.10.1 (11-02-2023)"
-#define _VERSION "0.10.1+56994f1 (11-02-2023)"
+#define _VERSION "0.10.1+0f35eb6 (11-02-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
