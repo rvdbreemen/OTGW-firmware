5163683c022333111018186a5b42272cb41cce1a
Robert van den Breemen <robert@vandenbreemen.net>
2023-02-11 01:22:40 +0100

    fixing configuration of actions

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 2042
-#define _VERSION_GITHASH "6cc0dde"
+#define _VERSION_BUILD 2043
+#define _VERSION_GITHASH "c8970c8"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "10-02-2023"
-#define _VERSION_TIME "21:11:23"
+#define _VERSION_DATE "11-02-2023"
+#define _VERSION_TIME "01:21:13"
 #define _SEMVER_CORE "0.10.1"
-#define _SEMVER_BUILD "0.10.1+2042"
-#define _SEMVER_GITHASH "0.10.1+6cc0dde"
-#define _SEMVER_FULL "0.10.1-beta+6cc0dde"
-#define _SEMVER_NOBUILD "0.10.1-beta (10-02-2023)"
-#define _VERSION "0.10.1-beta+6cc0dde (10-02-2023)"
+#define _SEMVER_BUILD "0.10.1+2043"
+#define _SEMVER_GITHASH "0.10.1+c8970c8"
+#define _SEMVER_FULL "0.10.1-beta+c8970c8"
+#define _SEMVER_NOBUILD "0.10.1-beta (11-02-2023)"
+#define _VERSION "0.10.1-beta+c8970c8 (11-02-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
