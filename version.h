d4d33c7894a023d92dad3812692c709dbac1d274
Robert van den Breemen <robert@vandenbreemen.net>
2023-02-13 21:43:43 +0100

    Fix for hostname to wifi (credits to @hvxl)

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 2044
-#define _VERSION_GITHASH "db010f2"
+#define _VERSION_BUILD 2046
+#define _VERSION_GITHASH "1d8f80b"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "11-02-2023"
-#define _VERSION_TIME "09:30:33"
+#define _VERSION_DATE "13-02-2023"
+#define _VERSION_TIME "19:11:23"
 #define _SEMVER_CORE "0.10.1"
-#define _SEMVER_BUILD "0.10.1+2044"
-#define _SEMVER_GITHASH "0.10.1+db010f2"
-#define _SEMVER_FULL "0.10.1+db010f2"
-#define _SEMVER_NOBUILD "0.10.1 (11-02-2023)"
-#define _VERSION "0.10.1+db010f2 (11-02-2023)"
+#define _SEMVER_BUILD "0.10.1+2046"
+#define _SEMVER_GITHASH "0.10.1+1d8f80b"
+#define _SEMVER_FULL "0.10.1+1d8f80b"
+#define _SEMVER_NOBUILD "0.10.1 (13-02-2023)"
+#define _VERSION "0.10.1+1d8f80b (13-02-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
