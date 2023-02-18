d84c4397a1d89a09c02b25cbb626a7072c3135cf
Robert van den Breemen <robert@vandenbreemen.net>
2023-02-18 20:26:02 +0100

    Trying to build a relase system

--- version.h
+++ version.h
@@ -2,6 +2,7 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 1
+<<<<<<< Updated upstream
 #define _VERSION_BUILD 2045
 #define _VERSION_GITHASH "867432a"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
@@ -13,4 +14,17 @@
 #define _SEMVER_FULL "0.10.1+867432a"
 #define _SEMVER_NOBUILD "0.10.1 (11-02-2023)"
 #define _VERSION "0.10.1+867432a (11-02-2023)"
+=======
+#define _VERSION_BUILD 2068
+#define _VERSION_GITHASH "d4d33c7"
+//#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
+#define _VERSION_DATE "18-02-2023"
+#define _VERSION_TIME "19:56:37"
+#define _SEMVER_CORE "0.10.1"
+#define _SEMVER_BUILD "0.10.1+2068"
+#define _SEMVER_GITHASH "0.10.1+d4d33c7"
+#define _SEMVER_FULL "0.10.1+d4d33c7"
+#define _SEMVER_NOBUILD "0.10.1 (18-02-2023)"
+#define _VERSION "0.10.1+d4d33c7 (18-02-2023)"
+>>>>>>> Stashed changes
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
