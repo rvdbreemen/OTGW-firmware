315751f33f25f39b3fd26570a679d46b5475ea36
Robert van den Breemen <robert@vandenbreemen.net>
2023-02-26 21:07:11 +0100

    Adding a little memory for use with larger settings... NTP longer FQDN fix

--- version.h
+++ version.h
@@ -2,29 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 1
-<<<<<<< Updated upstream
-#define _VERSION_BUILD 2045
-#define _VERSION_GITHASH "867432a"
+#define _VERSION_BUILD 2072
+#define _VERSION_GITHASH "141d6bf"
 //#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "11-02-2023"
-#define _VERSION_TIME "13:28:19"
+#define _VERSION_DATE "26-02-2023"
+#define _VERSION_TIME "21:02:42"
 #define _SEMVER_CORE "0.10.1"
-#define _SEMVER_BUILD "0.10.1+2045"
-#define _SEMVER_GITHASH "0.10.1+867432a"
-#define _SEMVER_FULL "0.10.1+867432a"
-#define _SEMVER_NOBUILD "0.10.1 (11-02-2023)"
-#define _VERSION "0.10.1+867432a (11-02-2023)"
-=======
-#define _VERSION_BUILD 2068
-#define _VERSION_GITHASH "d4d33c7"
-//#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "18-02-2023"
-#define _VERSION_TIME "19:56:37"
-#define _SEMVER_CORE "0.10.1"
-#define _SEMVER_BUILD "0.10.1+2068"
-#define _SEMVER_GITHASH "0.10.1+d4d33c7"
-#define _SEMVER_FULL "0.10.1+d4d33c7"
-#define _SEMVER_NOBUILD "0.10.1 (18-02-2023)"
-#define _VERSION "0.10.1+d4d33c7 (18-02-2023)"
->>>>>>> Stashed changes
+#define _SEMVER_BUILD "0.10.1+2072"
+#define _SEMVER_GITHASH "0.10.1+141d6bf"
+#define _SEMVER_FULL "0.10.1+141d6bf"
+#define _SEMVER_NOBUILD "0.10.1 (26-02-2023)"
+#define _VERSION "0.10.1+141d6bf (26-02-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
