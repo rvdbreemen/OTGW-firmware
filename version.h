f6e0bad30b2b51fa256e72f63423b5fdeebf0610
Robert van den Breemen <robert@vandenbreemen.net>
2023-03-16 22:49:59 +0100

    Turning off WTD during setup

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 2088
-#define _VERSION_GITHASH "50c3ed2"
-#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "14-03-2023"
-#define _VERSION_TIME "23:58:40"
-#define _SEMVER_CORE "0.10.2"
-#define _SEMVER_BUILD "0.10.2+2088"
-#define _SEMVER_GITHASH "0.10.2+50c3ed2"
-#define _SEMVER_FULL "0.10.2+50c3ed2"
-#define _SEMVER_NOBUILD "0.10.2 (14-03-2023)"
-#define _VERSION "0.10.2+50c3ed2 (14-03-2023)"
+#define _VERSION_BUILD 2089
+#define _VERSION_GITHASH "9b6336a"
+#define _VERSION_PRERELEASE beta
+#define _VERSION_DATE "16-03-2023"
+#define _VERSION_TIME "22:49:30"
+#define _SEMVER_CORE "0.10.3"
+#define _SEMVER_BUILD "0.10.3+2089"
+#define _SEMVER_GITHASH "0.10.3+9b6336a"
+#define _SEMVER_FULL "0.10.3-beta+9b6336a"
+#define _SEMVER_NOBUILD "0.10.3-beta (16-03-2023)"
+#define _VERSION "0.10.3-beta+9b6336a (16-03-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
