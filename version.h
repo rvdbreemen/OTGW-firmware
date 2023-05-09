4b94231959131f32070abe77692518ada9d3f949
Robert van den Breemen <robert@vandenbreemen.net>
2023-05-09 23:45:01 +0200

    removing renovate.json from tree

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 2094
-#define _VERSION_GITHASH "259eb21"
+#define _VERSION_BUILD 2095
+#define _VERSION_GITHASH "9000d6c"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "09-05-2023"
-#define _VERSION_TIME "23:26:24"
+#define _VERSION_TIME "23:31:54"
 #define _SEMVER_CORE "0.10.3"
-#define _SEMVER_BUILD "0.10.3+2094"
-#define _SEMVER_GITHASH "0.10.3+259eb21"
-#define _SEMVER_FULL "0.10.3-beta+259eb21"
+#define _SEMVER_BUILD "0.10.3+2095"
+#define _SEMVER_GITHASH "0.10.3+9000d6c"
+#define _SEMVER_FULL "0.10.3-beta+9000d6c"
 #define _SEMVER_NOBUILD "0.10.3-beta (09-05-2023)"
-#define _VERSION "0.10.3-beta+259eb21 (09-05-2023)"
+#define _VERSION "0.10.3-beta+9000d6c (09-05-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
