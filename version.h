97acf38a465afdad8a7af43ea920201683f3c980
Robert van den Breemen <robert@vandenbreemen.net>
2023-05-09 23:30:36 +0200

    cleanup transfer.dat in wrong place

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 2088
-#define _VERSION_GITHASH "50c3ed2"
-#define _VERSION_PRERELEASE beta  //uncomment to define prerelease labels: alpha - beta - rc
-#define _VERSION_DATE "14-03-2023"
-#define _VERSION_TIME "23:58:40"
-#define _SEMVER_CORE "0.10.2"
-#define _SEMVER_BUILD "0.10.2+2088"
-#define _SEMVER_GITHASH "0.10.2+50c3ed2"
-#define _SEMVER_FULL "0.10.2+50c3ed2"
-#define _SEMVER_NOBUILD "0.10.2 (14-03-2023)"
-#define _VERSION "0.10.2+50c3ed2 (14-03-2023)"
+#define _VERSION_BUILD 2094
+#define _VERSION_GITHASH "259eb21"
+#define _VERSION_PRERELEASE beta
+#define _VERSION_DATE "09-05-2023"
+#define _VERSION_TIME "23:26:24"
+#define _SEMVER_CORE "0.10.3"
+#define _SEMVER_BUILD "0.10.3+2094"
+#define _SEMVER_GITHASH "0.10.3+259eb21"
+#define _SEMVER_FULL "0.10.3-beta+259eb21"
+#define _SEMVER_NOBUILD "0.10.3-beta (09-05-2023)"
+#define _VERSION "0.10.3-beta+259eb21 (09-05-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
