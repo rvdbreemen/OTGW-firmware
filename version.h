dfb4bccfda9b9c09c76c03439e6950c07a600de6
Robert van den Breemen <robert@vandenbreemen.net>
2023-09-12 23:29:47 +0200

    Another fix by Sergantd

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 2096
-#define _VERSION_GITHASH "4b94231"
+#define _VERSION_BUILD 2097
+#define _VERSION_GITHASH "827df64"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "10-05-2023"
-#define _VERSION_TIME "06:54:37"
+#define _VERSION_DATE "12-09-2023"
+#define _VERSION_TIME "21:15:34"
 #define _SEMVER_CORE "0.10.3"
-#define _SEMVER_BUILD "0.10.3+2096"
-#define _SEMVER_GITHASH "0.10.3+4b94231"
-#define _SEMVER_FULL "0.10.3-beta+4b94231"
-#define _SEMVER_NOBUILD "0.10.3-beta (10-05-2023)"
-#define _VERSION "0.10.3-beta+4b94231 (10-05-2023)"
+#define _SEMVER_BUILD "0.10.3+2097"
+#define _SEMVER_GITHASH "0.10.3+827df64"
+#define _SEMVER_FULL "0.10.3-beta+827df64"
+#define _SEMVER_NOBUILD "0.10.3-beta (12-09-2023)"
+#define _VERSION "0.10.3-beta+827df64 (12-09-2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
