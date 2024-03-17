d29955ab48bf088514551ab1cdeae0dec2ab5818
Robert van den Breemen <robert@vandenbreemen.net>
2024-03-18 00:09:33 +0100

    Changes to make file, and build action. To simplify it

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 2101
-#define _VERSION_GITHASH "d99f272"
+#define _VERSION_BUILD 2102
+#define _VERSION_GITHASH "308ce13"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "17-03-2024"
-#define _VERSION_TIME "11:46:27"
+#define _VERSION_TIME "23:47:22"
 #define _SEMVER_CORE "0.10.3"
-#define _SEMVER_BUILD "0.10.3+2101"
-#define _SEMVER_GITHASH "0.10.3+d99f272"
-#define _SEMVER_FULL "0.10.3-beta+d99f272"
+#define _SEMVER_BUILD "0.10.3+2102"
+#define _SEMVER_GITHASH "0.10.3+308ce13"
+#define _SEMVER_FULL "0.10.3-beta+308ce13"
 #define _SEMVER_NOBUILD "0.10.3-beta (17-03-2024)"
-#define _VERSION "0.10.3-beta+d99f272 (17-03-2024)"
+#define _VERSION "0.10.3-beta+308ce13 (17-03-2024)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
