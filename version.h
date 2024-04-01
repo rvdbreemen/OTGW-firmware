ce7ea780907148bc7f9b4cc25962d25d45ecad8b
Robert van den Breemen <robert@vandenbreemen.net>
2024-04-01 09:26:25 +0200

    Fix APIGW, make it dependend on localURL (index.js) Fix status value for REST API, use OR instead of AND operator

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 2102
-#define _VERSION_GITHASH "308ce13"
+#define _VERSION_BUILD 2105
+#define _VERSION_GITHASH "1214611"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "17-03-2024"
-#define _VERSION_TIME "23:47:22"
+#define _VERSION_DATE "01-04-2024"
+#define _VERSION_TIME "09:17:10"
 #define _SEMVER_CORE "0.10.3"
-#define _SEMVER_BUILD "0.10.3+2102"
-#define _SEMVER_GITHASH "0.10.3+308ce13"
-#define _SEMVER_FULL "0.10.3-beta+308ce13"
-#define _SEMVER_NOBUILD "0.10.3-beta (17-03-2024)"
-#define _VERSION "0.10.3-beta+308ce13 (17-03-2024)"
+#define _SEMVER_BUILD "0.10.3+2105"
+#define _SEMVER_GITHASH "0.10.3+1214611"
+#define _SEMVER_FULL "0.10.3-beta+1214611"
+#define _SEMVER_NOBUILD "0.10.3-beta (01-04-2024)"
+#define _VERSION "0.10.3-beta+1214611 (01-04-2024)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
