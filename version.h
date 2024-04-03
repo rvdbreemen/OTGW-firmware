cb1f17ade950625c0ce54a3abd21d9402ab06ee1
Robert van den Breemen <robert@vandenbreemen.net>
2024-04-03 23:04:30 +0200

    Update release workflow to nodejs20

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 2106
-#define _VERSION_GITHASH "6752fdf"
+#define _VERSION_BUILD 2108
+#define _VERSION_GITHASH "3f0320f"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "02-04-2024"
-#define _VERSION_TIME "18:29:36"
+#define _VERSION_TIME "23:13:43"
 #define _SEMVER_CORE "0.10.3"
-#define _SEMVER_BUILD "0.10.3+2106"
-#define _SEMVER_GITHASH "0.10.3+6752fdf"
-#define _SEMVER_FULL "0.10.3-beta+6752fdf"
+#define _SEMVER_BUILD "0.10.3+2108"
+#define _SEMVER_GITHASH "0.10.3+3f0320f"
+#define _SEMVER_FULL "0.10.3-beta+3f0320f"
 #define _SEMVER_NOBUILD "0.10.3-beta (02-04-2024)"
-#define _VERSION "0.10.3-beta+6752fdf (02-04-2024)"
+#define _VERSION "0.10.3-beta+3f0320f (02-04-2024)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
