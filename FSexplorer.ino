32d9a1d46550624ad8758ee4381205ca4de298cb
Robert van den Breemen <robert@vandenbreemen.net>
2024-04-17 06:52:21 +0200

    Update to version 0.10.3 on file headers

--- FSexplorer.ino
+++ FSexplorer.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program : FSexplorer
-**  Version  : v0.10.2
+**  Version  : v0.10.3
 **
 **  Mostly stolen from https://www.arduinoforum.de/User-Fips
 **  For more information visit: https://fipsok.de
