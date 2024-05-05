3d063fffc482eb829ef2c75ecb9f7668f5ef06e7
Robert van den Breemen <robert@vandenbreemen.net>
2024-05-05 20:44:16 +0200

    Update file headers during development.

--- networkStuff.h
+++ networkStuff.h
@@ -2,7 +2,7 @@
 ***************************************************************************  
 **  Program : networkStuff.h
 **
-**  Version  : v0.10.3
+**  Version  : v0.10.4-beta-beta-beta
 **
 **  Copyright (c) 2021-2024 Robert van den Breemen
 **
