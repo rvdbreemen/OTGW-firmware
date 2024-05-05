5459bbb536866acf6730dbc541a682c3f5b2b3ff
Robert van den Breemen <robert@vandenbreemen.net>
2024-05-05 20:58:40 +0200

    Fixed the update script.

--- s0PulseCount.ino
+++ s0PulseCount.ino
@@ -1,7 +1,7 @@
 /* 
  ***************************************************************************  
  **  Program  : s0PulseCount
- **  Version  : v0.10.4-beta-beta-beta
+ **  Version  : v0.10.4-beta
  **
  **  Copyright (c) 2021-2024 Rob Roos / Robert van Breemen
  **     based on Framework ESP8266 from Willem Aandewiel
