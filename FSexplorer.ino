3cca1e5760a5f8b93ebc9cab97642779a90186b9
Robert van den Breemen <robert@vandenbreemen.net>
2025-12-31 00:37:30 +0100

    Refactor JSON status update handling to use esp8266httpupdateserver methods

--- FSexplorer.ino
+++ FSexplorer.ino
@@ -101,12 +101,8 @@ void setupFSexplorer(){
     // Buffer size must match JSON_STATUS_BUFFER_SIZE in OTGW-ModUpdateServer-impl.h
     constexpr size_t JSON_STATUS_BUFFER_SIZE = 512;
     char buf[JSON_STATUS_BUFFER_SIZE];
-    size_t len = updateStatusToJson(buf, sizeof(buf));
-    if (len > 0) {
-      httpServer.send(200, "application/json", buf);
-    } else {
-      httpServer.send(500, "application/json", "{\"error\":\"JSON generation error\"}");
-    }
+    esp8266httpupdateserver::updateStatusToJson(buf, sizeof(buf));
+    httpServer.send(200, "application/json", buf);
   });
   httpServer.on("/api/v0/update/events", HTTP_GET, []() {
     esp8266httpupdateserver::beginUpdateEventStream(httpServer);
