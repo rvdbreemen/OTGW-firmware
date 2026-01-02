7e36617b955465fa42589cf1912ba50ae4266495
Robert van den Breemen <robert@vandenbreemen.net>
2026-01-02 16:46:42 +0100

    Add Arduino 2 to .gitignore

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 0
 #define _VERSION_MINOR 10  
 #define _VERSION_PATCH 4
-#define _VERSION_BUILD 2156
-#define _VERSION_GITHASH "aa67a90"
+#define _VERSION_BUILD 2157
+#define _VERSION_GITHASH "3ea4ce3"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "02-01-2026"
-#define _VERSION_TIME "14:13:11"
+#define _VERSION_TIME "15:06:06"
 #define _SEMVER_CORE "0.10.4"
-#define _SEMVER_BUILD "0.10.4+2156"
-#define _SEMVER_GITHASH "0.10.4+aa67a90"
-#define _SEMVER_FULL "0.10.4-beta+aa67a90"
+#define _SEMVER_BUILD "0.10.4+2157"
+#define _SEMVER_GITHASH "0.10.4+3ea4ce3"
+#define _SEMVER_FULL "0.10.4-beta+3ea4ce3"
 #define _SEMVER_NOBUILD "0.10.4-beta (02-01-2026)"
-#define _VERSION "0.10.4-beta+aa67a90 (02-01-2026)"
+#define _VERSION "0.10.4-beta+3ea4ce3 (02-01-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
