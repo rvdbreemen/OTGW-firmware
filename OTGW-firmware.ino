a9031fc1e4f15ca9bf4a6b7a654c11d6c672ae9b
Robert van den Breemen <robert@vandenbreemen.net>
2026-01-06 08:25:20 +0100

    Bump version to 1.0.0-beta across all relevant files

--- OTGW-firmware.ino
+++ OTGW-firmware.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program  : OTGW-firmware.ino
-**  Version  : v0.10.4-beta
+**  Version  : v1.0.0-beta
 **
 **  Copyright (c) 2021-2024 Robert van den Breemen
 **
--- sensors_ext.ino
+++ sensors_ext.ino
@@ -1,6 +1,6 @@
 /*
 **  Program  : output_ext.ino
-**  Version  : v0.10.4-beta
+**  Version  : v1.0.0-beta
 **
 **  Copyright (c) 2021-2024 Robert van den Breemen
 **  Contributed by Sjorsjuhmaniac
