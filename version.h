f7a44dbaee4a3a1525e9a04c019806ab4551430d
Robert van den Breemen <robert@vandenbreemen.net>
2026-01-07 21:31:31 +0100

    Remove LittleFS format endpoint and related UI elements; update version information

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 0
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2244
-#define _VERSION_GITHASH "65ef122"
+#define _VERSION_BUILD 2246
+#define _VERSION_GITHASH "cfabfd3"
 #define _VERSION_PRERELEASE rc3
 #define _VERSION_DATE "07-01-2026"
-#define _VERSION_TIME "18:52:06"
+#define _VERSION_TIME "20:28:32"
 #define _SEMVER_CORE "1.0.0"
-#define _SEMVER_BUILD "1.0.0+2244"
-#define _SEMVER_GITHASH "1.0.0+65ef122"
-#define _SEMVER_FULL "1.0.0-rc3+65ef122"
+#define _SEMVER_BUILD "1.0.0+2246"
+#define _SEMVER_GITHASH "1.0.0+cfabfd3"
+#define _SEMVER_FULL "1.0.0-rc3+cfabfd3"
 #define _SEMVER_NOBUILD "1.0.0-rc3 (07-01-2026)"
-#define _VERSION "1.0.0-rc3+65ef122 (07-01-2026)"
+#define _VERSION "1.0.0-rc3+cfabfd3 (07-01-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
