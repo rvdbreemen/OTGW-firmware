c9def957c96867845dc3ee335432ab869ad6f734
Robert van den Breemen <robert@vandenbreemen.net>
2026-01-07 19:57:30 +0100

    Refactor JSON logging macros into helper functions for improved clarity and maintainability; update version information to reflect recent changes

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 0
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2243
-#define _VERSION_GITHASH "2bbb1ae"
+#define _VERSION_BUILD 2245
+#define _VERSION_GITHASH "65ef122"
 #define _VERSION_PRERELEASE rc3
 #define _VERSION_DATE "07-01-2026"
-#define _VERSION_TIME "18:48:33"
+#define _VERSION_TIME "18:56:09"
 #define _SEMVER_CORE "1.0.0"
-#define _SEMVER_BUILD "1.0.0+2243"
-#define _SEMVER_GITHASH "1.0.0+2bbb1ae"
-#define _SEMVER_FULL "1.0.0-rc3+2bbb1ae"
+#define _SEMVER_BUILD "1.0.0+2245"
+#define _SEMVER_GITHASH "1.0.0+65ef122"
+#define _SEMVER_FULL "1.0.0-rc3+65ef122"
 #define _SEMVER_NOBUILD "1.0.0-rc3 (07-01-2026)"
-#define _VERSION "1.0.0-rc3+2bbb1ae (07-01-2026)"
+#define _VERSION "1.0.0-rc3+65ef122 (07-01-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
