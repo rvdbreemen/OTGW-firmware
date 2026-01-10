6562b6eca36538d46068ea1e64db5f5f840e9db5
Robert van den Breemen <robert@vandenbreemen.net>
2026-01-10 13:16:58 +0100

    Update versioning details and enhance flash script automation

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 0
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2323
-#define _VERSION_GITHASH "c83dec9"
+#define _VERSION_BUILD 2324
+#define _VERSION_GITHASH "1ebf304"
 #define _VERSION_PRERELEASE rc3
 #define _VERSION_DATE "10-01-2026"
-#define _VERSION_TIME "11:50:48"
+#define _VERSION_TIME "12:15:03"
 #define _SEMVER_CORE "1.0.0"
-#define _SEMVER_BUILD "1.0.0+2323"
-#define _SEMVER_GITHASH "1.0.0+c83dec9"
-#define _SEMVER_FULL "1.0.0-rc3+c83dec9"
+#define _SEMVER_BUILD "1.0.0+2324"
+#define _SEMVER_GITHASH "1.0.0+1ebf304"
+#define _SEMVER_FULL "1.0.0-rc3+1ebf304"
 #define _SEMVER_NOBUILD "1.0.0-rc3 (10-01-2026)"
-#define _VERSION "1.0.0-rc3+c83dec9 (10-01-2026)"
+#define _VERSION "1.0.0-rc3+1ebf304 (10-01-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
