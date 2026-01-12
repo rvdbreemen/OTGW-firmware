32b00e38a21eea32aa2a9d4bed0e3d3d84be69b0
Robert van den Breemen <robert@vandenbreemen.net>
2026-01-12 20:29:07 +0100

    Refactor error messages to use snprintf_P for improved memory handling; update version to 1.0.0-rc3+565f2d1

--- OTGW-firmware.h
+++ OTGW-firmware.h
@@ -119,6 +119,7 @@ bool      bPSmode = false;  //default to PS=0 mode
 
 //All things that are settings 
 char      settingHostname[41] = _HOSTNAME;
+char      settingAdminPassword[41] = "";
 
 //MQTT settings
 bool      statusMQTTconnection = false; 
