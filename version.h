d0dbb3f50d2f27e7981bf55583855e99f6c5fa68
Robert van den Breemen <robert@vandenbreemen.net>
2026-01-12 07:34:33 +0100

    Update version to 1.0.0-rc3+4423a01 and refactor sendJsonOTmonObj to utilize PROGMEM for improved memory efficiency

--- version.h
+++ version.h
@@ -2,15 +2,15 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 0
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2332
-#define _VERSION_GITHASH "98ac46c"
+#define _VERSION_BUILD 2334
+#define _VERSION_GITHASH "4423a01"
 #define _VERSION_PRERELEASE rc3
 #define _VERSION_DATE "12-01-2026"
-#define _VERSION_TIME "05:48:06"
+#define _VERSION_TIME "06:33:16"
 #define _SEMVER_CORE "1.0.0"
-#define _SEMVER_BUILD "1.0.0+2332"
-#define _SEMVER_GITHASH "1.0.0+98ac46c"
-#define _SEMVER_FULL "1.0.0-rc3+98ac46c"
+#define _SEMVER_BUILD "1.0.0+2334"
+#define _SEMVER_GITHASH "1.0.0+4423a01"
+#define _SEMVER_FULL "1.0.0-rc3+4423a01"
 #define _SEMVER_NOBUILD "1.0.0-rc3 (12-01-2026)"
-#define _VERSION "1.0.0-rc3+98ac46c (12-01-2026)"
+#define _VERSION "1.0.0-rc3+4423a01 (12-01-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
