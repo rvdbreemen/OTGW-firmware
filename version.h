ac3ef8c6050e87e4a42aa91b6bb029cd7abc333b
Robert van den Breemen <robert@vandenbreemen.net>
2026-01-15 02:18:35 +0100

    Update version information to 1.0.0-rc3+bf27d91, increment build number and adjust timestamps

--- version.h
+++ version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 0
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2364
-#define _VERSION_GITHASH "589ab3f"
+#define _VERSION_BUILD 2365
+#define _VERSION_GITHASH "bf27d91"
 #define _VERSION_PRERELEASE rc3
 #define _VERSION_DATE "15-01-2026"
-#define _VERSION_TIME "01:06:41"
+#define _VERSION_TIME "01:18:25"
 #define _SEMVER_CORE "1.0.0"
-#define _SEMVER_BUILD "1.0.0+2364"
-#define _SEMVER_GITHASH "1.0.0+589ab3f"
-#define _SEMVER_FULL "1.0.0-rc3+589ab3f"
+#define _SEMVER_BUILD "1.0.0+2365"
+#define _SEMVER_GITHASH "1.0.0+bf27d91"
+#define _SEMVER_FULL "1.0.0-rc3+bf27d91"
 #define _SEMVER_NOBUILD "1.0.0-rc3 (15-01-2026)"
-#define _VERSION "1.0.0-rc3+589ab3f (15-01-2026)"
+#define _VERSION "1.0.0-rc3+bf27d91 (15-01-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
