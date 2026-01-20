4b17f52560306f2b86dce6f444fa8d8778ca6d43
Robert van den Breemen <robert@vandenbreemen.net>
2026-01-20 23:53:18 +0100

    Update version to 1.0.0-rc4+3d920d8 and optimize memory usage with PROGMEM for string literals

--- version.h
+++ version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 0
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2398
-#define _VERSION_GITHASH "80c263b"
+#define _VERSION_BUILD 2396
+#define _VERSION_GITHASH "3d920d8"
 #define _VERSION_PRERELEASE rc4
 #define _VERSION_DATE "20-01-2026"
-#define _VERSION_TIME "22:46:25"
+#define _VERSION_TIME "22:52:52"
 #define _SEMVER_CORE "1.0.0"
-#define _SEMVER_BUILD "1.0.0+2398"
-#define _SEMVER_GITHASH "1.0.0+80c263b"
-#define _SEMVER_FULL "1.0.0-rc4+80c263b"
+#define _SEMVER_BUILD "1.0.0+2396"
+#define _SEMVER_GITHASH "1.0.0+3d920d8"
+#define _SEMVER_FULL "1.0.0-rc4+3d920d8"
 #define _SEMVER_NOBUILD "1.0.0-rc4 (20-01-2026)"
-#define _VERSION "1.0.0-rc4+80c263b (20-01-2026)"
+#define _VERSION "1.0.0-rc4+3d920d8 (20-01-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
