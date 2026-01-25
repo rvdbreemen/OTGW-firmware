deb23d2bb64a747b6f18ec45fa246afdc54b7dd0
Robert van den Breemen <robert@vandenbreemen.net>
2026-01-25 20:56:34 +0100

    Bump version to v1.0.0-rc5 across all relevant files

--- OTGW-Core.ino
+++ OTGW-Core.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program  : OTGW-Core.ino
-**  Version  : v1.0.0-rc4
+**  Version  : v1.0.0-rc5
 **
 **  Copyright (c) 2021-2026 Robert van den Breemen
 **  Borrowed from OpenTherm library from: 
--- version.h
+++ version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 0
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2417
-#define _VERSION_GITHASH "3ea1d59"
+#define _VERSION_BUILD 2418
+#define _VERSION_GITHASH "0f128c8"
 #define _VERSION_PRERELEASE rc5
 #define _VERSION_DATE "25-01-2026"
-#define _VERSION_TIME "19:53:03"
+#define _VERSION_TIME "19:56:18"
 #define _SEMVER_CORE "1.0.0"
-#define _SEMVER_BUILD "1.0.0+2417"
-#define _SEMVER_GITHASH "1.0.0+3ea1d59"
-#define _SEMVER_FULL "1.0.0-rc4+3ea1d59"
-#define _SEMVER_NOBUILD "1.0.0-rc4 (25-01-2026)"
-#define _VERSION "1.0.0-rc4+3ea1d59 (25-01-2026)"
+#define _SEMVER_BUILD "1.0.0+2418"
+#define _SEMVER_GITHASH "1.0.0+0f128c8"
+#define _SEMVER_FULL "1.0.0-rc5+0f128c8"
+#define _SEMVER_NOBUILD "1.0.0-rc5 (25-01-2026)"
+#define _VERSION "1.0.0-rc5+0f128c8 (25-01-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
