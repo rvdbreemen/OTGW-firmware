42df1130b03b1429ecb001594f6016cfae3189a9
Robert van den Breemen <robert@vandenbreemen.net>
2026-01-25 20:55:54 +0100

    Update prerelease version to rc5 in version.h

--- version.h
+++ version.h
@@ -7,7 +7,7 @@
 #define _VERSION_PATCH 0
 #define _VERSION_BUILD 2417
 #define _VERSION_GITHASH "3ea1d59"
-#define _VERSION_PRERELEASE rc4
+#define _VERSION_PRERELEASE rc5
 #define _VERSION_DATE "25-01-2026"
 #define _VERSION_TIME "19:53:03"
 #define _SEMVER_CORE "1.0.0"
