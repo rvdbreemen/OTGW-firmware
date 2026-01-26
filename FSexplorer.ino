52ac039dcad219ba8c5a663f239b1994e7ba9fa2
Robert van den Breemen <robert@vandenbreemen.net>
2026-01-26 22:10:04 +0100

    Update version to rc6 in version.h

--- FSexplorer.ino
+++ FSexplorer.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program : FSexplorer
-**  Version  : v1.0.0-rc5
+**  Version  : v1.0.0-rc6
 **
 **  Mostly stolen from https://www.arduinoforum.de/User-Fips
 **  For more information visit: https://fipsok.de
--- MQTTstuff.ino
+++ MQTTstuff.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program  : MQTTstuff
-**  Version  : v1.0.0-rc5
+**  Version  : v1.0.0-rc6
 **
 **  Copyright (c) 2021-2026 Robert van den Breemen
 **      Modified version from (c) 2020 Willem Aandewiel
