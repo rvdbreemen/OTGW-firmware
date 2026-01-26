36d1dfb78110814b8761e3a45a7eff06c9196e33
Robert van den Breemen <robert@vandenbreemen.net>
2026-01-26 22:10:04 +0100

    Update version to rc6 in version.h

--- helperStuff.ino
+++ helperStuff.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program  : helperStuff
-**  Version  : v1.0.0-rc5
+**  Version  : v1.0.0-rc6
 **
 **  Copyright (c) 2021-2026 Robert van den Breemen
 **     based on Framework ESP8266 from Willem Aandewiel
--- outputs_ext.ino
+++ outputs_ext.ino
@@ -1,6 +1,6 @@
 /*********
 **  Program  : output_ext.ino
-**  Version  : v1.0.0-rc5
+**  Version  : v1.0.0-rc6
 **
 **  Copyright (c) 2021-2026 Robert van den Breemen
 **  Contributed by Sjorsjuhmaniac
