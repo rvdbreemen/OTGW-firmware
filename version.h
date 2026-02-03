201e97af448c1ab73bed9520cb861656d02c04c3
Robert van den Breemen <robert@vandenbreemen.net>
2026-02-04 00:51:54 +0100

    Update version information to 1.0.0-rc6+95ded9c and fix hash values

--- version.h
+++ version.h
@@ -5,37 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 0
 #define _VERSION_PATCH 0
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-#define _VERSION_BUILD 2493
-#define _VERSION_GITHASH "90923a0"
+#define _VERSION_BUILD 2497
+#define _VERSION_GITHASH "95ded9c"
 #define _VERSION_PRERELEASE rc6
 #define _VERSION_DATE "03-02-2026"
-#define _VERSION_TIME "23:24:50"
+#define _VERSION_TIME "23:51:13"
 #define _SEMVER_CORE "1.0.0"
-#define _SEMVER_BUILD "1.0.0+2493"
-=======
-#define _VERSION_BUILD 2494
-#define _VERSION_GITHASH "90923a0"
-#define _VERSION_PRERELEASE rc6
-#define _VERSION_DATE "03-02-2026"
-#define _VERSION_TIME "23:36:17"
-#define _SEMVER_CORE "1.0.0"
-#define _SEMVER_BUILD "1.0.0+2494"
->>>>>>> Stashed changes
-=======
-#define _VERSION_BUILD 2494
-#define _VERSION_GITHASH "90923a0"
-#define _VERSION_PRERELEASE rc6
-#define _VERSION_DATE "03-02-2026"
-#define _VERSION_TIME "23:36:17"
-#define _SEMVER_CORE "1.0.0"
-#define _SEMVER_BUILD "1.0.0+2494"
->>>>>>> Stashed changes
-#define _SEMVER_GITHASH "1.0.0+90923a0"
-#define _SEMVER_FULL "1.0.0-rc6+90923a0"
+#define _SEMVER_BUILD "1.0.0+2497"
+#define _SEMVER_GITHASH "1.0.0+95ded9c"
+#define _SEMVER_FULL "1.0.0-rc6+95ded9c"
 #define _SEMVER_NOBUILD "1.0.0-rc6 (03-02-2026)"
-#define _VERSION "1.0.0-rc6+90923a0 (03-02-2026)"
+#define _VERSION "1.0.0-rc6+95ded9c (03-02-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
