1e5ec5dc04d3e53312b7366d1bc35b802f82bac1
Robert van den Breemen <robert@vandenbreemen.net>
2026-02-04 00:17:37 +0100

    Update version information to 1.0.0-rc6+3d77357 and enhance device reboot handling

--- version.h
+++ version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 0
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2489
-#define _VERSION_GITHASH "0789c03"
+#define _VERSION_BUILD 2496
+#define _VERSION_GITHASH "3d77357"
 #define _VERSION_PRERELEASE rc6
 #define _VERSION_DATE "03-02-2026"
-#define _VERSION_TIME "22:51:43"
+#define _VERSION_TIME "23:11:48"
 #define _SEMVER_CORE "1.0.0"
-#define _SEMVER_BUILD "1.0.0+2489"
-#define _SEMVER_GITHASH "1.0.0+0789c03"
-#define _SEMVER_FULL "1.0.0-rc6+0789c03"
+#define _SEMVER_BUILD "1.0.0+2496"
+#define _SEMVER_GITHASH "1.0.0+3d77357"
+#define _SEMVER_FULL "1.0.0-rc6+3d77357"
 #define _SEMVER_NOBUILD "1.0.0-rc6 (03-02-2026)"
-#define _VERSION "1.0.0-rc6+0789c03 (03-02-2026)"
+#define _VERSION "1.0.0-rc6+3d77357 (03-02-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
