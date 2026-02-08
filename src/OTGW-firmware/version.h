d09013645b4b8159f9bd18a1161542338f352fb1
Robert van den Breemen <robert@vandenbreemen.net>
2026-02-08 21:38:25 +0100

    Update version.h to set prerelease to beta and remove minor version

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -3,11 +3,11 @@
 #define VERSION_H
 
 #define _VERSION_MAJOR 1
-#define _VERSION_MINOR 0
+#define _VERSION_MINOR 
 #define _VERSION_PATCH 0
 #define _VERSION_BUILD 2528
 #define _VERSION_GITHASH "c03a635"
-//#define _VERSION_PRERELEASE rc7
+#define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "08-02-2026"
 #define _VERSION_TIME "20:06:47"
 #define _SEMVER_CORE "1.0.0"
