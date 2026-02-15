16720a2af99a16c4316450efb65ef6a04e5769e7
Robert van den Breemen <robert@vandenbreemen.net>
2026-02-15 22:53:54 +0100

    Update version information and refine sensor simulation condition

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 1
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2550
-#define _VERSION_GITHASH "26c504e"
+#define _VERSION_BUILD 2552
+#define _VERSION_GITHASH "f7d88c1"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "15-02-2026"
-#define _VERSION_TIME "21:46:55"
+#define _VERSION_TIME "21:52:37"
 #define _SEMVER_CORE "1.1.0"
-#define _SEMVER_BUILD "1.1.0+2550"
-#define _SEMVER_GITHASH "1.1.0+26c504e"
-#define _SEMVER_FULL "1.1.0-beta+26c504e"
+#define _SEMVER_BUILD "1.1.0+2552"
+#define _SEMVER_GITHASH "1.1.0+f7d88c1"
+#define _SEMVER_FULL "1.1.0-beta+f7d88c1"
 #define _SEMVER_NOBUILD "1.1.0-beta (15-02-2026)"
-#define _VERSION "1.1.0-beta+26c504e (15-02-2026)"
+#define _VERSION "1.1.0-beta+f7d88c1 (15-02-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
