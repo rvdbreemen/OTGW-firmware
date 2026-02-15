985f8771b006fa3b825b0b75f5fb19feab541607
Robert van den Breemen <robert@vandenbreemen.net>
2026-02-15 20:04:45 +0100

    Bump version to v1.1.0-beta across multiple files

--- src/OTGW-firmware/MQTTstuff.ino
+++ src/OTGW-firmware/MQTTstuff.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program  : MQTTstuff
-**  Version  : v1.0.1
+**  Version  : v1.1.0-beta
 **
 **  Copyright (c) 2021-2026 Robert van den Breemen
 **      Modified version from (c) 2020 Willem Aandewiel
