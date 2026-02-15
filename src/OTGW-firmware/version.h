eb4319e0427536735bf0b17858281025ec7d0f94
Robert van den Breemen <robert@vandenbreemen.net>
2026-02-15 19:47:42 +0100

    Refactor build process to use temporary directory for artifacts and update versioning information

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 1
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2539
-#define _VERSION_GITHASH "3e2bf5e"
+#define _VERSION_BUILD 2540
+#define _VERSION_GITHASH "dc9dd0f"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "11-02-2026"
-#define _VERSION_TIME "21:24:10"
+#define _VERSION_DATE "15-02-2026"
+#define _VERSION_TIME "18:42:53"
 #define _SEMVER_CORE "1.1.0"
-#define _SEMVER_BUILD "1.1.0+2539"
-#define _SEMVER_GITHASH "1.1.0+3e2bf5e"
-#define _SEMVER_FULL "1.1.0-beta+3e2bf5e"
-#define _SEMVER_NOBUILD "1.1.0-beta (11-02-2026)"
-#define _VERSION "1.1.0-beta+3e2bf5e (11-02-2026)"
+#define _SEMVER_BUILD "1.1.0+2540"
+#define _SEMVER_GITHASH "1.1.0+dc9dd0f"
+#define _SEMVER_FULL "1.1.0-beta+dc9dd0f"
+#define _SEMVER_NOBUILD "1.1.0-beta (15-02-2026)"
+#define _VERSION "1.1.0-beta+dc9dd0f (15-02-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
