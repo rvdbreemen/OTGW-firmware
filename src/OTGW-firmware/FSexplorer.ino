2eb0c91e3ed7d893d182808f600300cb9c329277
Robert van den Breemen <robert@vandenbreemen.net>
2026-02-16 23:13:26 +0100

    chore: update version to v1.2.0-beta across multiple files

--- src/OTGW-firmware/FSexplorer.ino
+++ src/OTGW-firmware/FSexplorer.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program : FSexplorer
-**  Version  : v1.1.0-beta
+**  Version  : v1.2.0-beta
 **
 **  Mostly stolen from https://www.arduinoforum.de/User-Fips
 **  For more information visit: https://fipsok.de
