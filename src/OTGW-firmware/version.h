1e1813f9c18c38c6d56073ad50a61367ebb789ab
Robert van den Breemen <robert@vandenbreemen.net>
2026-02-21 23:13:43 +0100

    Enhance WebSocket event logging for PS mode changes and serial buffer overflows; update version information

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 1
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2573
-#define _VERSION_GITHASH "3679ce5"
+#define _VERSION_BUILD 2575
+#define _VERSION_GITHASH "ed8cc26"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "21-02-2026"
-#define _VERSION_TIME "21:25:57"
+#define _VERSION_TIME "22:05:55"
 #define _SEMVER_CORE "1.1.0"
-#define _SEMVER_BUILD "1.1.0+2573"
-#define _SEMVER_GITHASH "1.1.0+3679ce5"
-#define _SEMVER_FULL "1.1.0-beta+3679ce5"
+#define _SEMVER_BUILD "1.1.0+2575"
+#define _SEMVER_GITHASH "1.1.0+ed8cc26"
+#define _SEMVER_FULL "1.1.0-beta+ed8cc26"
 #define _SEMVER_NOBUILD "1.1.0-beta (21-02-2026)"
-#define _VERSION "1.1.0-beta+3679ce5 (21-02-2026)"
+#define _VERSION "1.1.0-beta+ed8cc26 (21-02-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
