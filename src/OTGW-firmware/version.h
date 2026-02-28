70a0764886c242d192fc7036903077d3432302b3
Robert van den Breemen <robert@vandenbreemen.net>
2026-02-28 19:00:54 +0100

    chore: update version to 1.2.0-beta+68e63da and increment build number to 2632

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,27 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 2
 #define _VERSION_PATCH 0
-<<<<<<< HEAD
-#define _VERSION_BUILD 2627
-#define _VERSION_GITHASH "bffd597"
+#define _VERSION_BUILD 2632
+#define _VERSION_GITHASH "1d7bb63"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "28-02-2026"
-#define _VERSION_TIME "17:47:45"
+#define _VERSION_TIME "17:58:44"
 #define _SEMVER_CORE "1.2.0"
-#define _SEMVER_BUILD "1.2.0+2627"
-=======
-#define _VERSION_BUILD 2627
-#define _VERSION_GITHASH "bffd597"
-#define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "28-02-2026"
-#define _VERSION_TIME "17:47:45"
-#define _SEMVER_CORE "1.2.0"
-#define _SEMVER_BUILD "1.2.0+2627"
->>>>>>> 5ffd73a7f84d06452770c1eb1583836415e52d23
-#define _SEMVER_GITHASH "1.2.0+bffd597"
-#define _SEMVER_FULL "1.2.0-beta+bffd597"
+#define _SEMVER_BUILD "1.2.0+2632"
+#define _SEMVER_GITHASH "1.2.0+1d7bb63"
+#define _SEMVER_FULL "1.2.0-beta+1d7bb63"
 #define _SEMVER_NOBUILD "1.2.0-beta (28-02-2026)"
-#define _VERSION "1.2.0-beta+bffd597 (28-02-2026)"
+#define _VERSION "1.2.0-beta+1d7bb63 (28-02-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
