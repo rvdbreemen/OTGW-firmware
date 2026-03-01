cff04890f74358a6c6f5007ae520ef46b94977d4
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-01 22:03:30 +0100

    fix: update writeSettings behavior and improve file handling; update version to 1.3.0-beta+6969a7d

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 3
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2620
-#define _VERSION_GITHASH "e06bad7"
+#define _VERSION_BUILD 2622
+#define _VERSION_GITHASH "cc66801"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "28-02-2026"
-#define _VERSION_TIME "23:58:25"
+#define _VERSION_DATE "01-03-2026"
+#define _VERSION_TIME "20:58:13"
 #define _SEMVER_CORE "1.3.0"
-#define _SEMVER_BUILD "1.3.0+2620"
-#define _SEMVER_GITHASH "1.3.0+e06bad7"
-#define _SEMVER_FULL "1.3.0-beta+e06bad7"
-#define _SEMVER_NOBUILD "1.3.0-beta (28-02-2026)"
-#define _VERSION "1.3.0-beta+e06bad7 (28-02-2026)"
+#define _SEMVER_BUILD "1.3.0+2622"
+#define _SEMVER_GITHASH "1.3.0+cc66801"
+#define _SEMVER_FULL "1.3.0-beta+cc66801"
+#define _SEMVER_NOBUILD "1.3.0-beta (01-03-2026)"
+#define _VERSION "1.3.0-beta+cc66801 (01-03-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
