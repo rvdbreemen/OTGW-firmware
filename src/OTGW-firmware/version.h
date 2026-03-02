54c42284897911dc2e4c024e6f51059b3752c932
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-02 22:21:47 +0100

    Bump OTGW firmware build to 2642
    Increment OTGW firmware build to 2642 and update git hash to "ca23a6a". Refresh version date/time to 02-03-2026 21:19:50 and update related semver macros and version.hash so firmware metadata remains consistent.

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 2
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2641
-#define _VERSION_GITHASH "7dedf78"
+#define _VERSION_BUILD 2642
+#define _VERSION_GITHASH "0e35632"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "28-02-2026"
-#define _VERSION_TIME "21:48:29"
+#define _VERSION_DATE "02-03-2026"
+#define _VERSION_TIME "21:19:50"
 #define _SEMVER_CORE "1.2.0"
-#define _SEMVER_BUILD "1.2.0+2641"
-#define _SEMVER_GITHASH "1.2.0+7dedf78"
-#define _SEMVER_FULL "1.2.0-beta+7dedf78"
-#define _SEMVER_NOBUILD "1.2.0-beta (28-02-2026)"
-#define _VERSION "1.2.0-beta+7dedf78 (28-02-2026)"
+#define _SEMVER_BUILD "1.2.0+2642"
+#define _SEMVER_GITHASH "1.2.0+0e35632"
+#define _SEMVER_FULL "1.2.0-beta+0e35632"
+#define _SEMVER_NOBUILD "1.2.0-beta (02-03-2026)"
+#define _VERSION "1.2.0-beta+0e35632 (02-03-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
