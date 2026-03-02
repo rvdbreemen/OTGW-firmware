b5024ac4dc04cb83efe494b12c96a4c68289ad8d
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-02 23:04:27 +0100

    Prepare v1.2.0 release: update docs & version
    Finalize v1.2.0 release by removing beta/pre-release wording and updating release metadata. README and RELEASE_NOTES updated to reflect stable v1.2.0 (condensed prior sections and fixed wording/typos), RELEASE_GITHUB_1.2.0.md added for the GitHub release body, and docs/RELEASE_PROCESS.md added with a pre-release checklist and steps. Update src/OTGW-firmware/version.h to remove the -beta pre-release suffix and set the stable semver strings.

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -13,9 +13,9 @@
 #define _SEMVER_CORE "1.2.0"
 #define _SEMVER_BUILD "1.2.0+2643"
 #define _SEMVER_GITHASH "1.2.0+9ec0540"
-#define _SEMVER_FULL "1.2.0-beta+9ec0540"
-#define _SEMVER_NOBUILD "1.2.0-beta (02-03-2026)"
-#define _VERSION "1.2.0-beta+9ec0540 (02-03-2026)"
+#define _SEMVER_FULL "1.2.0+9ec0540"
+#define _SEMVER_NOBUILD "1.2.0 (02-03-2026)"
+#define _VERSION "1.2.0+9ec0540 (02-03-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
