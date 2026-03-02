594171e646b6320c720ba4c8e05fcdd5b648a137
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-02 23:12:14 +0100

    Release v1.2.0
    Update project version from v1.2.0-beta to v1.2.0 across firmware sources and web assets. Bump build metadata and git hash in version.h (build 2645, githash a0badbe), update version.timestamp and version.hash file. This prepares the codebase and bundled UI files for the 1.2.0 release.

--- src/OTGW-firmware/FSexplorer.ino
+++ src/OTGW-firmware/FSexplorer.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program : FSexplorer
-**  Version  : v1.2.0-beta
+**  Version  : v1.2.0
 **
 **  Mostly stolen from https://www.arduinoforum.de/User-Fips
 **  For more information visit: https://fipsok.de
