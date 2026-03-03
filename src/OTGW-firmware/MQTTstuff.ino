d79e4214c89d08e9f1dd4dfe9294072328a3856b
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-04 00:37:52 +0100

    Bump version to v1.3.0-beta across all relevant files

--- src/OTGW-firmware/MQTTstuff.ino
+++ src/OTGW-firmware/MQTTstuff.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program  : MQTTstuff
-**  Version  : v1.2.0
+**  Version  : v1.3.0-beta
 **
 **  Copyright (c) 2021-2026 Robert van den Breemen
 **      Modified version from (c) 2020 Willem Aandewiel
--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -3,19 +3,19 @@
 #define VERSION_H
 
 #define _VERSION_MAJOR 1
-#define _VERSION_MINOR 2
+#define _VERSION_MINOR 3
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2645
-#define _VERSION_GITHASH "a0badbe"
-// #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "02-03-2026"
-#define _VERSION_TIME "22:11:58"
-#define _SEMVER_CORE "1.2.0"
-#define _SEMVER_BUILD "1.2.0+2645"
-#define _SEMVER_GITHASH "1.2.0+a0badbe"
-#define _SEMVER_FULL "1.2.0+a0badbe"
-#define _SEMVER_NOBUILD "1.2.0 (02-03-2026)"
-#define _VERSION "1.2.0+a0badbe (02-03-2026)"
+#define _VERSION_BUILD 2647
+#define _VERSION_GITHASH "bac5b82"
+#define _VERSION_PRERELEASE beta
+#define _VERSION_DATE "03-03-2026"
+#define _VERSION_TIME "23:35:28"
+#define _SEMVER_CORE "1.3.0"
+#define _SEMVER_BUILD "1.3.0+2647"
+#define _SEMVER_GITHASH "1.3.0+bac5b82"
+#define _SEMVER_FULL "1.3.0-beta+bac5b82"
+#define _SEMVER_NOBUILD "1.3.0-beta (03-03-2026)"
+#define _VERSION "1.3.0-beta+bac5b82 (03-03-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
