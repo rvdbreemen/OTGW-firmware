f13789d94e421140bf4eaad691c10367bb5c81da
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-05 02:14:34 +0100

    Update PS mode handling and version information

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 3
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2650
-#define _VERSION_GITHASH "e4e996e"
+#define _VERSION_BUILD 2653
+#define _VERSION_GITHASH "0e7fcd7"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "05-03-2026"
-#define _VERSION_TIME "00:52:13"
+#define _VERSION_TIME "01:13:42"
 #define _SEMVER_CORE "1.3.0"
-#define _SEMVER_BUILD "1.3.0+2650"
-#define _SEMVER_GITHASH "1.3.0+e4e996e"
-#define _SEMVER_FULL "1.3.0-beta+e4e996e"
+#define _SEMVER_BUILD "1.3.0+2653"
+#define _SEMVER_GITHASH "1.3.0+0e7fcd7"
+#define _SEMVER_FULL "1.3.0-beta+0e7fcd7"
 #define _SEMVER_NOBUILD "1.3.0-beta (05-03-2026)"
-#define _VERSION "1.3.0-beta+e4e996e (05-03-2026)"
+#define _VERSION "1.3.0-beta+0e7fcd7 (05-03-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
