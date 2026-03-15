2c94f597c72f66863c49706859eb4a5a742afb3f
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-15 23:01:36 +0100

    feat: implement GPIO conflict detection and update version to 1.3.0-beta+8832102

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 3
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2708
-#define _VERSION_GITHASH "37c1dca"
+#define _VERSION_BUILD 2719
+#define _VERSION_GITHASH "e986477"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "14-03-2026"
-#define _VERSION_TIME "23:06:56"
+#define _VERSION_DATE "15-03-2026"
+#define _VERSION_TIME "22:00:26"
 #define _SEMVER_CORE "1.3.0"
-#define _SEMVER_BUILD "1.3.0+2708"
-#define _SEMVER_GITHASH "1.3.0+37c1dca"
-#define _SEMVER_FULL "1.3.0-beta+37c1dca"
-#define _SEMVER_NOBUILD "1.3.0-beta (14-03-2026)"
-#define _VERSION "1.3.0-beta+37c1dca (14-03-2026)"
+#define _SEMVER_BUILD "1.3.0+2719"
+#define _SEMVER_GITHASH "1.3.0+e986477"
+#define _SEMVER_FULL "1.3.0-beta+e986477"
+#define _SEMVER_NOBUILD "1.3.0-beta (15-03-2026)"
+#define _VERSION "1.3.0-beta+e986477 (15-03-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
