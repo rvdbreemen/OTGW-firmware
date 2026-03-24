aa3adda60acc75d4ca55611165853ef42c049293
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-24 01:01:01 +0100

    Update dark theme styles and increment version to 1.3.0-rc3

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 3
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2786
-#define _VERSION_GITHASH "6abf3f0"
+#define _VERSION_BUILD 2787
+#define _VERSION_GITHASH "3ee649f"
 #define _VERSION_PRERELEASE rc3
 #define _VERSION_DATE "23-03-2026"
-#define _VERSION_TIME "23:38:15"
+#define _VERSION_TIME "23:47:05"
 #define _SEMVER_CORE "1.3.0"
-#define _SEMVER_BUILD "1.3.0+2786"
-#define _SEMVER_GITHASH "1.3.0+6abf3f0"
-#define _SEMVER_FULL "1.3.0-rc3+6abf3f0"
+#define _SEMVER_BUILD "1.3.0+2787"
+#define _SEMVER_GITHASH "1.3.0+3ee649f"
+#define _SEMVER_FULL "1.3.0-rc3+3ee649f"
 #define _SEMVER_NOBUILD "1.3.0-rc3 (23-03-2026)"
-#define _VERSION "1.3.0-rc3+6abf3f0 (23-03-2026)"
+#define _VERSION "1.3.0-rc3+3ee649f (23-03-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
