ed6c4f6bab35510bbf56da44c3135fcc86cb2b95
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-25 23:03:11 +0100

    Enhance PIC settings handling and UI updates
    - Add caching mechanism for PIC settings with localStorage
    - Implement refresh timer for periodic updates of PIC settings
    - Update CSS styles for displaying PIC settings states
    - Ensure Unicode output compatibility for Windows consoles
    - Bump version to 1.3.0-rc4 with updated build and git hash

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 3
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2795
-#define _VERSION_GITHASH "58601ea"
+#define _VERSION_BUILD 2802
+#define _VERSION_GITHASH "282418f"
 #define _VERSION_PRERELEASE rc4
 #define _VERSION_DATE "25-03-2026"
-#define _VERSION_TIME "06:26:06"
+#define _VERSION_TIME "21:22:51"
 #define _SEMVER_CORE "1.3.0"
-#define _SEMVER_BUILD "1.3.0+2795"
-#define _SEMVER_GITHASH "1.3.0+58601ea"
-#define _SEMVER_FULL "1.3.0-rc4+58601ea"
+#define _SEMVER_BUILD "1.3.0+2802"
+#define _SEMVER_GITHASH "1.3.0+282418f"
+#define _SEMVER_FULL "1.3.0-rc4+282418f"
 #define _SEMVER_NOBUILD "1.3.0-rc4 (25-03-2026)"
-#define _VERSION "1.3.0-rc4+58601ea (25-03-2026)"
+#define _VERSION "1.3.0-rc4+282418f (25-03-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
