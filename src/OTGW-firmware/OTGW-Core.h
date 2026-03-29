47eb606be7566524976fce4b36e7d43c7d6f9df0
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-29 21:26:19 +0200

    release: update version to v1.3.2, removing beta tag across multiple files

--- src/OTGW-firmware/OTGW-Core.h
+++ src/OTGW-firmware/OTGW-Core.h
@@ -1,7 +1,7 @@
 /*
 ***************************************************************************  
 **  Program  : Header file: OTGW-Core.h 
-**  Version  : v1.3.2-beta
+**  Version  : v1.3.2
 **
 **  Copyright (c) 2021-2026 Robert van den Breemen
 **  Borrowed from OpenTherm library from: 
--- src/OTGW-firmware/OTGW-Core.ino
+++ src/OTGW-firmware/OTGW-Core.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program  : OTGW-Core.ino
-**  Version  : v1.3.2-beta
+**  Version  : v1.3.2
 **
 **  Copyright (c) 2021-2026 Robert van den Breemen
 **  Borrowed from OpenTherm library from: 
