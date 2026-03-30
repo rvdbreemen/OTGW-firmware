26b00fb294c5c293706a8fe771e82d4eebc3391e
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-31 00:22:23 +0200

    feat: update version to 1.3.3-beta+01d9ab8 and adjust build number

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 3
 #define _VERSION_PATCH 3
-#define _VERSION_BUILD 2917
-#define _VERSION_GITHASH "fad2ce7"
+#define _VERSION_BUILD 2918
+#define _VERSION_GITHASH "ae4487a"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "30-03-2026"
-#define _VERSION_TIME "22:05:25"
+#define _VERSION_TIME "22:14:07"
 #define _SEMVER_CORE "1.3.3"
-#define _SEMVER_BUILD "1.3.3+2917"
-#define _SEMVER_GITHASH "1.3.3+fad2ce7"
-#define _SEMVER_FULL "1.3.3-beta+fad2ce7"
+#define _SEMVER_BUILD "1.3.3+2918"
+#define _SEMVER_GITHASH "1.3.3+ae4487a"
+#define _SEMVER_FULL "1.3.3-beta+ae4487a"
 #define _SEMVER_NOBUILD "1.3.3-beta (30-03-2026)"
-#define _VERSION "1.3.3-beta+fad2ce7 (30-03-2026)"
+#define _VERSION "1.3.3-beta+ae4487a (30-03-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
