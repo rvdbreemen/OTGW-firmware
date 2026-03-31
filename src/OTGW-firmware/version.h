27717683e2050798e5e9de6ab09c3fbe2acc9cf5
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-31 23:51:03 +0200

    feat: update OpenTherm message types and improve serial error handling for ESP32/ESP8266

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 4
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2951
-#define _VERSION_GITHASH "8cb8eaf"
+#define _VERSION_BUILD 2953
+#define _VERSION_GITHASH "9c1e4e5"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "31-03-2026"
-#define _VERSION_TIME "21:08:01"
+#define _VERSION_TIME "21:27:30"
 #define _SEMVER_CORE "1.4.0"
-#define _SEMVER_BUILD "1.4.0+2951"
-#define _SEMVER_GITHASH "1.4.0+8cb8eaf"
-#define _SEMVER_FULL "1.4.0-beta+8cb8eaf"
+#define _SEMVER_BUILD "1.4.0+2953"
+#define _SEMVER_GITHASH "1.4.0+9c1e4e5"
+#define _SEMVER_FULL "1.4.0-beta+9c1e4e5"
 #define _SEMVER_NOBUILD "1.4.0-beta (31-03-2026)"
-#define _VERSION "1.4.0-beta+8cb8eaf (31-03-2026)"
+#define _VERSION "1.4.0-beta+9c1e4e5 (31-03-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
