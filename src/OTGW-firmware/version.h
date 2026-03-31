08801c90ef1367fbfd7d60c6d32be0f0c7b0bac7
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-31 07:53:44 +0200

    feat: update version to 1.4.0-beta across all relevant files

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 4
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2943
-#define _VERSION_GITHASH "6c2f478"
+#define _VERSION_BUILD 2946
+#define _VERSION_GITHASH "6678a69"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "30-03-2026"
-#define _VERSION_TIME "21:23:17"
+#define _VERSION_DATE "01-04-2026"
+#define _VERSION_TIME "23:01:34"
 #define _SEMVER_CORE "1.4.0"
-#define _SEMVER_BUILD "1.4.0+2943"
-#define _SEMVER_GITHASH "1.4.0+6c2f478"
-#define _SEMVER_FULL "1.4.0-beta+6c2f478"
-#define _SEMVER_NOBUILD "1.4.0-beta (30-03-2026)"
-#define _VERSION "1.4.0-beta+6c2f478 (30-03-2026)"
+#define _SEMVER_BUILD "1.4.0+2946"
+#define _SEMVER_GITHASH "1.4.0+6678a69"
+#define _SEMVER_FULL "1.4.0-beta+6678a69"
+#define _SEMVER_NOBUILD "1.4.0-beta (01-04-2026)"
+#define _VERSION "1.4.0-beta+6678a69 (01-04-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
