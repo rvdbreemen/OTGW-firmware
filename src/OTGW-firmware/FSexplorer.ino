8b2c91dbff80cbc2cb6e1393deeab10037dc73e9
Robert van den Breemen <robert@vandenbreemen.net>
2026-03-31 07:53:44 +0200

    feat: update version to 1.4.0-beta across all relevant files

--- src/OTGW-firmware/FSexplorer.ino
+++ src/OTGW-firmware/FSexplorer.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program : FSexplorer
-**  Version  : v1.3.5
+**  Version  : v1.4.0-beta
 **
 **  Mostly stolen from https://www.arduinoforum.de/User-Fips
 **  For more information visit: https://fipsok.de
--- src/OTGW-firmware/MQTTstuff.ino
+++ src/OTGW-firmware/MQTTstuff.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program  : MQTTstuff
-**  Version  : v1.3.5
+**  Version  : v1.4.0-beta
 **
 **  Copyright (c) 2021-2026 Robert van den Breemen
 **      Modified version from (c) 2020 Willem Aandewiel
