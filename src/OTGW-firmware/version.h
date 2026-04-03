e1fef75fd33f6445b327ed34ada11ba230d8d9d7
Robert van den Breemen <robert@vandenbreemen.net>
2026-04-03 06:47:24 +0200

    feat: enhance bootloader and partition table search logic, and improve OTA upload handling

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 4
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 2953
-#define _VERSION_GITHASH "9c1e4e5"
+#define _VERSION_BUILD 2966
+#define _VERSION_GITHASH "a2236f9"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "31-03-2026"
-#define _VERSION_TIME "21:27:30"
+#define _VERSION_DATE "06-04-2026"
+#define _VERSION_TIME "00:02:07"
 #define _SEMVER_CORE "1.4.0"
-#define _SEMVER_BUILD "1.4.0+2953"
-#define _SEMVER_GITHASH "1.4.0+9c1e4e5"
-#define _SEMVER_FULL "1.4.0-beta+9c1e4e5"
-#define _SEMVER_NOBUILD "1.4.0-beta (31-03-2026)"
-#define _VERSION "1.4.0-beta+9c1e4e5 (31-03-2026)"
+#define _SEMVER_BUILD "1.4.0+2966"
+#define _SEMVER_GITHASH "1.4.0+a2236f9"
+#define _SEMVER_FULL "1.4.0-beta+a2236f9"
+#define _SEMVER_NOBUILD "1.4.0-beta (06-04-2026)"
+#define _VERSION "1.4.0-beta+a2236f9 (06-04-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
