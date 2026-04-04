8fa898f33c4e892b3f03ac83a50b4777f657418e
Robert van den Breemen <robert@vandenbreemen.net>
2026-04-05 00:34:25 +0200

    chore: update version information and enhance .gitignore for additional files

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 3
 #define _VERSION_PATCH 5
-#define _VERSION_BUILD 2931
-#define _VERSION_GITHASH "656ccab"
+#define _VERSION_BUILD 2933
+#define _VERSION_GITHASH "67e5d8e"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "03-04-2026"
-#define _VERSION_TIME "05:24:13"
+#define _VERSION_DATE "04-04-2026"
+#define _VERSION_TIME "22:29:19"
 #define _SEMVER_CORE "1.3.5"
-#define _SEMVER_BUILD "1.3.5+2931"
-#define _SEMVER_GITHASH "1.3.5+656ccab"
-#define _SEMVER_FULL "1.3.5-beta+656ccab"
-#define _SEMVER_NOBUILD "1.3.5-beta (03-04-2026)"
-#define _VERSION "1.3.5-beta+656ccab (03-04-2026)"
+#define _SEMVER_BUILD "1.3.5+2933"
+#define _SEMVER_GITHASH "1.3.5+67e5d8e"
+#define _SEMVER_FULL "1.3.5-beta+67e5d8e"
+#define _SEMVER_NOBUILD "1.3.5-beta (04-04-2026)"
+#define _VERSION "1.3.5-beta+67e5d8e (04-04-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
