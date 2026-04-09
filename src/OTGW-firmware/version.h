1a5f44ffa2598c7f099f791ea43194c9099cad43
Robert van den Breemen <robert@vandenbreemen.net>
2026-04-09 22:08:41 +0200

    chore: bump version to v1.3.8-beta for fix-issue-maxtset-boiler-source

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -4,18 +4,18 @@
 
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 3
-#define _VERSION_PATCH 7
-#define _VERSION_BUILD 2942
-#define _VERSION_GITHASH "281b8a0"
+#define _VERSION_PATCH 8
+#define _VERSION_BUILD 2943
+#define _VERSION_GITHASH "e97f556"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "08-04-2026"
-#define _VERSION_TIME "20:38:25"
-#define _SEMVER_CORE "1.3.7"
-#define _SEMVER_BUILD "1.3.7+2942"
-#define _SEMVER_GITHASH "1.3.7+281b8a0"
-#define _SEMVER_FULL "1.3.7-beta+281b8a0"
-#define _SEMVER_NOBUILD "1.3.7-beta (08-04-2026)"
-#define _VERSION "1.3.7-beta+281b8a0 (08-04-2026)"
+#define _VERSION_DATE "09-04-2026"
+#define _VERSION_TIME "20:06:59"
+#define _SEMVER_CORE "1.3.8"
+#define _SEMVER_BUILD "1.3.8+2943"
+#define _SEMVER_GITHASH "1.3.8+e97f556"
+#define _SEMVER_FULL "1.3.8-beta+e97f556"
+#define _SEMVER_NOBUILD "1.3.8-beta (09-04-2026)"
+#define _VERSION "1.3.8-beta+e97f556 (09-04-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
