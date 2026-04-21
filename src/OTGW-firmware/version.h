b8c5e4cb9b02357c08995fd03be68a28cce52f46
Robert van den Breemen <robert@vandenbreemen.net>
2026-04-22 00:51:16 +0200

    chore: update version hash after backport build (2.0.0-beta+5ca1d4f)

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 2
 #define _VERSION_MINOR 0
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 3487
-#define _VERSION_GITHASH "41a83ee"
+#define _VERSION_BUILD 3507
+#define _VERSION_GITHASH "e94b4e7"
 #define _VERSION_PRERELEASE beta
-#define _VERSION_DATE "19-04-2026"
-#define _VERSION_TIME "19:38:22"
+#define _VERSION_DATE "21-04-2026"
+#define _VERSION_TIME "22:48:33"
 #define _SEMVER_CORE "2.0.0"
-#define _SEMVER_BUILD "2.0.0+3487"
-#define _SEMVER_GITHASH "2.0.0+41a83ee"
-#define _SEMVER_FULL "2.0.0-beta+41a83ee"
-#define _SEMVER_NOBUILD "2.0.0-beta (19-04-2026)"
-#define _VERSION "2.0.0-beta+41a83ee (19-04-2026)"
+#define _SEMVER_BUILD "2.0.0+3507"
+#define _SEMVER_GITHASH "2.0.0+e94b4e7"
+#define _SEMVER_FULL "2.0.0-beta+e94b4e7"
+#define _SEMVER_NOBUILD "2.0.0-beta (21-04-2026)"
+#define _VERSION "2.0.0-beta+e94b4e7 (21-04-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
