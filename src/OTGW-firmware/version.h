f3cf209dcb32eec3e5a46d1e07a9e94c8a4a4b91
Robert van den Breemen <robert@vandenbreemen.net>
2026-04-24 15:41:47 +0200

    chore(lts): bump version to 1.5.0-beta on LTS-1.4.x-on-2.7.4 branch
    Positioning the Core 2.7.4 LTS line as the future 1.5.x release.
    Initial test results on this branch show materially better heap and
    lower memory fragmentation than the 1.4.x Core 3.1.2 line, so this
    branch is now the candidate mainline if A/B validation confirms.
    
    MAJOR.MINOR.PATCH = 1.5.0. Build.py autoinc-semver.py will regenerate
    the derived _SEMVER_* strings on the next build.
    
    Branch naming stays LTS-1.4.x-on-2.7.4 until the 1.5.0 release lands;
    version header is the authoritative identifier from now on.

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -3,19 +3,19 @@
 #define VERSION_H
 
 #define _VERSION_MAJOR 1
-#define _VERSION_MINOR 4
-#define _VERSION_PATCH 2
-#define _VERSION_BUILD 3184
-#define _VERSION_GITHASH "5d69dc8"
+#define _VERSION_MINOR 5
+#define _VERSION_PATCH 0
+#define _VERSION_BUILD 3198
+#define _VERSION_GITHASH "257acb4"
 #define _VERSION_PRERELEASE beta
 #define _VERSION_DATE "24-04-2026"
-#define _VERSION_TIME "11:37:51"
+#define _VERSION_TIME "13:25:34"
 #define _SEMVER_CORE "1.4.2"
-#define _SEMVER_BUILD "1.4.2+3184"
-#define _SEMVER_GITHASH "1.4.2+5d69dc8"
-#define _SEMVER_FULL "1.4.2-beta+5d69dc8"
+#define _SEMVER_BUILD "1.4.2+3198"
+#define _SEMVER_GITHASH "1.4.2+257acb4"
+#define _SEMVER_FULL "1.4.2-beta+257acb4"
 #define _SEMVER_NOBUILD "1.4.2-beta (24-04-2026)"
-#define _VERSION "1.4.2-beta+5d69dc8 (24-04-2026)"
+#define _VERSION "1.4.2-beta+257acb4 (24-04-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
