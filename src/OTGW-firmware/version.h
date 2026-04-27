f494b895ea1ce82139a168a78e495f59c339e116
Robert van den Breemen <robert@vandenbreemen.net>
2026-04-27 23:49:42 +0200

    chore(build): sync autoinc semver outputs

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 2
 #define _VERSION_MINOR 0
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 3719
-#define _VERSION_GITHASH "4c825de"
+#define _VERSION_BUILD 3747
+#define _VERSION_GITHASH "940ac49"
 #define _VERSION_PRERELEASE alpha
 #define _VERSION_DATE "27-04-2026"
-#define _VERSION_TIME "19:57:52"
+#define _VERSION_TIME "21:46:25"
 #define _SEMVER_CORE "2.0.0"
-#define _SEMVER_BUILD "2.0.0+3719"
-#define _SEMVER_GITHASH "2.0.0+4c825de"
-#define _SEMVER_FULL "2.0.0-alpha+4c825de"
+#define _SEMVER_BUILD "2.0.0+3747"
+#define _SEMVER_GITHASH "2.0.0+940ac49"
+#define _SEMVER_FULL "2.0.0-alpha+940ac49"
 #define _SEMVER_NOBUILD "2.0.0-alpha (27-04-2026)"
-#define _VERSION "2.0.0-alpha+4c825de (27-04-2026)"
+#define _VERSION "2.0.0-alpha+940ac49 (27-04-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
