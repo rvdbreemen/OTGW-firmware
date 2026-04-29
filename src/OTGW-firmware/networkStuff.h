6b9b11468f1ecc8538d3cce722de7b256715c976
Robert van den Breemen <robert@vandenbreemen.net>
2026-04-30 01:36:33 +0200

    chore(release): bump version to v1.5.0-beta.4 and update related files

--- src/OTGW-firmware/networkStuff.h
+++ src/OTGW-firmware/networkStuff.h
@@ -1,7 +1,7 @@
 /*
 ***************************************************************************
 **  Program : networkStuff.h
-**  Version  : v1.5.0-beta.3
+**  Version  : v1.5.0-beta.4
 **
 **  Copyright (c) 2021-2026 Robert van den Breemen
 **     based on Framework ESP8266 from Willem Aandewiel
--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 5
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 3211
-#define _VERSION_GITHASH "d5589f4"
-#define _VERSION_PRERELEASE beta.3
-#define _VERSION_DATE "28-04-2026"
-#define _VERSION_TIME "20:27:16"
+#define _VERSION_BUILD 3214
+#define _VERSION_GITHASH "297c6eb"
+#define _VERSION_PRERELEASE beta.4
+#define _VERSION_DATE "29-04-2026"
+#define _VERSION_TIME "23:35:52"
 #define _SEMVER_CORE "1.5.0"
-#define _SEMVER_BUILD "1.5.0+3211"
-#define _SEMVER_GITHASH "1.5.0+d5589f4"
-#define _SEMVER_FULL "1.5.0-beta.3+d5589f4"
-#define _SEMVER_NOBUILD "1.5.0-beta.3 (28-04-2026)"
-#define _VERSION "1.5.0-beta.3+d5589f4 (28-04-2026)"
+#define _SEMVER_BUILD "1.5.0+3214"
+#define _SEMVER_GITHASH "1.5.0+297c6eb"
+#define _SEMVER_FULL "1.5.0-beta.4+297c6eb"
+#define _SEMVER_NOBUILD "1.5.0-beta.4 (29-04-2026)"
+#define _VERSION "1.5.0-beta.4+297c6eb (29-04-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
