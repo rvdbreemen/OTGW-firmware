5ef559165b3b492910b1e43f6a65d9ebc918a204
Robert van den Breemen <robert@vandenbreemen.net>
2026-04-30 00:38:58 +0200

    chore(release): bump build to 1.5.0-beta.4+c2cb58f (3214)

--- src/OTGW-firmware/FSexplorer.ino
+++ src/OTGW-firmware/FSexplorer.ino
@@ -1,7 +1,7 @@
 /* 
 ***************************************************************************  
 **  Program : FSexplorer
-**  Version  : v1.5.0-beta.3
+**  Version  : v1.5.0-beta.4
 **
 **  Mostly stolen from https://www.arduinoforum.de/User-Fips
 **  For more information visit: https://fipsok.de
--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 5
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 3211
-#define _VERSION_GITHASH "d5589f4"
-#define _VERSION_PRERELEASE beta.3
-#define _VERSION_DATE "28-04-2026"
-#define _VERSION_TIME "20:27:16"
+#define _VERSION_BUILD 3214
+#define _VERSION_GITHASH "c2cb58f"
+#define _VERSION_PRERELEASE beta.4
+#define _VERSION_DATE "29-04-2026"
+#define _VERSION_TIME "22:35:14"
 #define _SEMVER_CORE "1.5.0"
-#define _SEMVER_BUILD "1.5.0+3211"
-#define _SEMVER_GITHASH "1.5.0+d5589f4"
-#define _SEMVER_FULL "1.5.0-beta.3+d5589f4"
-#define _SEMVER_NOBUILD "1.5.0-beta.3 (28-04-2026)"
-#define _VERSION "1.5.0-beta.3+d5589f4 (28-04-2026)"
+#define _SEMVER_BUILD "1.5.0+3214"
+#define _SEMVER_GITHASH "1.5.0+c2cb58f"
+#define _SEMVER_FULL "1.5.0-beta.4+c2cb58f"
+#define _SEMVER_NOBUILD "1.5.0-beta.4 (29-04-2026)"
+#define _VERSION "1.5.0-beta.4+c2cb58f (29-04-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
