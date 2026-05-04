a8cd706b5b3ccabf12962d7b75aef61de87ed3f4
Robert van den Breemen <robert@vandenbreemen.net>
2026-05-04 18:45:12 +0200

    chore(version): bump prerelease to 1.5.0-beta.11

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 5
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 3242
-#define _VERSION_GITHASH "9f33d79"
-#define _VERSION_PRERELEASE beta.10
+#define _VERSION_BUILD 3246
+#define _VERSION_GITHASH "8cf181d"
+#define _VERSION_PRERELEASE beta.11
 #define _VERSION_DATE "04-05-2026"
-#define _VERSION_TIME "11:15:35"
+#define _VERSION_TIME "16:45:12"
 #define _SEMVER_CORE "1.5.0"
-#define _SEMVER_BUILD "1.5.0+3242"
-#define _SEMVER_GITHASH "1.5.0+9f33d79"
-#define _SEMVER_FULL "1.5.0-beta.10+9f33d79"
-#define _SEMVER_NOBUILD "1.5.0-beta.10 (04-05-2026)"
-#define _VERSION "1.5.0-beta.10+9f33d79 (04-05-2026)"
+#define _SEMVER_BUILD "1.5.0+3246"
+#define _SEMVER_GITHASH "1.5.0+8cf181d"
+#define _SEMVER_FULL "1.5.0-beta.11+8cf181d"
+#define _SEMVER_NOBUILD "1.5.0-beta.11 (04-05-2026)"
+#define _VERSION "1.5.0-beta.11+8cf181d (04-05-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
