138a517b7bc0eb3322bfc0af59caba7782f3eb40
Robert van den Breemen <robert@vandenbreemen.net>
2026-05-20 07:29:27 +0200

    fix(mqtt): surface silently-dropped set-commands in default debug stream (#602)

--- src/OTGW-firmware/version.h
+++ src/OTGW-firmware/version.h
@@ -5,17 +5,17 @@
 #define _VERSION_MAJOR 1
 #define _VERSION_MINOR 6
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 3341
-#define _VERSION_GITHASH "3da7c87"
-#define _VERSION_PRERELEASE beta.1
+#define _VERSION_BUILD 3342
+#define _VERSION_GITHASH "447915d"
+#define _VERSION_PRERELEASE beta.2
 #define _VERSION_DATE "19-05-2026"
-#define _VERSION_TIME "16:52:04"
+#define _VERSION_TIME "20:13:42"
 #define _SEMVER_CORE "1.6.0"
-#define _SEMVER_BUILD "1.6.0+3341"
-#define _SEMVER_GITHASH "1.6.0+3da7c87"
-#define _SEMVER_FULL "1.6.0-beta.1+3da7c87"
-#define _SEMVER_NOBUILD "1.6.0-beta.1 (19-05-2026)"
-#define _VERSION "1.6.0-beta.1+3da7c87 (19-05-2026)"
+#define _SEMVER_BUILD "1.6.0+3342"
+#define _SEMVER_GITHASH "1.6.0+447915d"
+#define _SEMVER_FULL "1.6.0-beta.2+447915d"
+#define _SEMVER_NOBUILD "1.6.0-beta.2 (19-05-2026)"
+#define _VERSION "1.6.0-beta.2+447915d (19-05-2026)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
 
 #endif // VERSION_H
